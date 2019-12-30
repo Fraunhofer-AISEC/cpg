2b520e9bd7315ac5a28d225f67d33a8feef19e46
Christian Banse <christian.banse@aisec.fraunhofer.de>
2019-12-30 14:17:23 +0100

    Added even more tests

--- src/test/resources/components/ForEachStmt.java
+++ src/test/resources/components/ForEachStmt.java
@@ -1,11 +1,9 @@
 import java.util.Arrays;
 import java.util.List;
 
-class ForForEachStmt {
-
+class ForEachStmt {
   public static void main(String[] args) {
     List<String> ls = Arrays.asList(args);
-    for (int i = 0; i < args.length; i++) System.out.println(args[i]);
     for (String s : ls) System.out.println(s);
   }
 }
