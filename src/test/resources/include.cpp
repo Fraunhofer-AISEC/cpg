a2445834a8d881f68ec6a99f439345cc74d2ab65
Banse, Christian <christian.banse@aisec.fraunhofer.de>
2020-01-21 10:38:27 +0100

    Added a (failing) test case to demonstrate issue #41

---
+++ src/test/resources/include.cpp
@@ -0,0 +1,5 @@
+#include "include.h"
+
+int main() {
+  SomeClass c = new SomeClass();
+}
---
+++ src/test/resources/include.h
@@ -0,0 +1,13 @@
+// this header file needs to be larger to check if
+// we do not run
+// into issues when parsing
+// the regions and code
+// that is why we are producing some extra content here just to make it larger.
+
+// Did i mention this needs to be larger?
+
+class SomeClass {
+
+  SomeClass();
+
+};
