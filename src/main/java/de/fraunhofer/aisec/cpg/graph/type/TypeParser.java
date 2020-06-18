b2e67a5f8b701d397b1fa0d3395b3283d3dd5c2a
vfsrfs <vfsrfs@vfsrfs.me>
2020-06-18 17:25:04 +0200

    print language

--- src/main/java/de/fraunhofer/aisec/cpg/graph/type/TypeParser.java
+++ src/main/java/de/fraunhofer/aisec/cpg/graph/type/TypeParser.java
@@ -623,7 +623,7 @@ public class TypeParser {
    * @return new type representing the type string
    */
   public static Type createFrom(String type, boolean resolveAlias) {
-    System.out.println("Parser: " + type);
+    System.out.println("Parser: " + type + "-> " + getLanguage());
     // Check if Problems during Parsing
     if (type.contains("?")
         || type.contains("org.eclipse.cdt.internal.core.dom.parser.ProblemType@")
