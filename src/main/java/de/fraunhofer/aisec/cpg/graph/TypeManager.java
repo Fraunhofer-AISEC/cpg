b1862f018542fa5f7dc557fce029803f5e0dbde0
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2020-07-16 13:21:48 +0200

    Fix: Nullability of TypeManager.frontend is unclear. There is code that assumes it to be non-null, but it is also explicitly set to null somewhere else.

--- src/main/java/de/fraunhofer/aisec/cpg/graph/TypeManager.java
+++ src/main/java/de/fraunhofer/aisec/cpg/graph/TypeManager.java
@@ -38,6 +38,8 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,14 +51,16 @@ public class TypeManager {
       List.of("byte", "short", "int", "long", "float", "double", "boolean", "char");
   private static final Pattern funPointerPattern =
       Pattern.compile("\\(?\\*(?<alias>[^()]+)\\)?\\(.*\\)");
-  private static TypeManager INSTANCE = new TypeManager();
+  @NonNull private static TypeManager INSTANCE = new TypeManager();
 
   public enum Language {
     JAVA,
     CXX
   }
 
-  private Map<String, RecordDeclaration> typeToRecord = new HashMap<>();
+  @NonNull private Map<String, RecordDeclaration> typeToRecord = new HashMap<>();
+
+  @NonNull
   private Map<Type, List<Type>> typeState =
       new HashMap<>(); // Stores all the unique types ObjectType as Key and Reference-/PointerTypes
   // as Values
@@ -97,7 +101,7 @@ public class TypeManager {
     return INSTANCE;
   }
 
-  public void setLanguageFrontend(LanguageFrontend frontend) {
+  public void setLanguageFrontend(@NonNull LanguageFrontend frontend) {
     this.frontend = frontend;
   }
 
@@ -290,6 +294,7 @@ public class TypeManager {
     }
   }
 
+  @Nullable
   public LanguageFrontend getFrontend() {
     return frontend;
   }
