aafab6745a4a86b7a89702ed059e207cd0ad9118
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2020-07-28 09:24:12 +0200

    Performance: Added TODOs to 2 hotspots.

--- src/main/java/de/fraunhofer/aisec/cpg/graph/TypeManager.java
+++ src/main/java/de/fraunhofer/aisec/cpg/graph/TypeManager.java
@@ -133,6 +133,7 @@ public class TypeManager {
   }
 
   private Set<Type> unwrapTypes(Collection<Type> types, WrapState wrapState) {
+    // TODO Performance: This method is called very often (for each setType()) and does four iterations over "types". Reduce number of iterations.
     Set<Type> original = new HashSet<>(types);
     Set<Type> unwrappedTypes = new HashSet<>();
     int depth = 0;
