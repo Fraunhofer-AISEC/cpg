18d4b21c383981a7332dbdfb4794296149d6c0ec
vfsrfs <christian.presa-schnell@aisec.fraunhofer.de>
2021-03-14 19:09:34 +0100

    remove unnecessary imports

--- src/main/java/de/fraunhofer/aisec/cpg/graph/types/ParameterizedType.java
+++ src/main/java/de/fraunhofer/aisec/cpg/graph/types/ParameterizedType.java
@@ -1,16 +1,5 @@
 package de.fraunhofer.aisec.cpg.graph.types;
 
-import static de.fraunhofer.aisec.cpg.graph.edge.PropertyEdge.unwrap;
-
-import de.fraunhofer.aisec.cpg.graph.SubGraph;
-import de.fraunhofer.aisec.cpg.graph.TemplateParameter;
-import de.fraunhofer.aisec.cpg.graph.edge.Properties;
-import de.fraunhofer.aisec.cpg.graph.edge.PropertyEdge;
-import de.fraunhofer.aisec.cpg.graph.statements.expressions.Expression;
-import java.util.ArrayList;
-import java.util.List;
-import org.neo4j.ogm.annotation.Relationship;
-
 /**
  * ParameterizedTypes describe types, that are passed as Paramters to Classes E.g. uninitialized
  * generics in the graph are represented as ParameterizedTypes
