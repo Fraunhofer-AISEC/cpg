02b02a0ccfa15dcae2e7a6362834423a1d20fb10
Christian Banse <christian.banse@aisec.fraunhofer.de>
2021-03-19 20:12:04 +0100

    triyng to build it using CI

--- src/main/java/de/fraunhofer/aisec/cpg/frontends/python/JepSingleton.kt
+++ src/main/java/de/fraunhofer/aisec/cpg/frontends/python/JepSingleton.kt
@@ -5,9 +5,8 @@ import jep.MainInterpreter
 // Jep's configuration must be set before the first instance is created. Later calls to setJepLibraryPath and co result in failures.
 object JepSingleton {
     init {
-        // TODO
-        MainInterpreter.setJepLibraryPath("/home/maximilian/.virtualenvs/jep/lib/python3.9/site-packages/jep/libjep.so")
-        // val pyConfig = PyConfig().setPythonHome("/home/maximilian/.virtualenvs/jep/")
-        // MainInterpreter.setInitParams(pyConfig)
+        // do not set it for CI
+        // TODO: check different paths or get it via environment variable on non-CI
+        //MainInterpreter.setJepLibraryPath(System.getProperty("user.home") + "/.virtualenvs/jep/lib/python3.9/site-packages/jep/libjep.so")
     }
 }
