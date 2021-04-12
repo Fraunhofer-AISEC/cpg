57512d5eceefafd5abd5f65041aa7e058f73b79b
vfsrfs <christian.presa-schnell@aisec.fraunhofer.de>
2021-04-12 16:53:14 +0200

    documentation for edge properties

--- src/main/java/de/fraunhofer/aisec/cpg/graph/edge/Properties.java
+++ src/main/java/de/fraunhofer/aisec/cpg/graph/edge/Properties.java
@@ -1,5 +1,15 @@
 package de.fraunhofer.aisec.cpg.graph.edge;
 
+/**
+ * INDEX:(int) Indicates the position in a list of edges
+ *
+ * <p>BRANCH:(boolean) If we have multiple EOG edges the branch property indicates which EOG edge
+ * leads to true branch (expression evaluated to true) or the false branch (e.g. with an if/else
+ * contidion)
+ *
+ * <p>DEFAULT:(boolean) Indicates which arguments edge of a CallExpression leads to a default
+ * argument
+ */
 public enum Properties {
   INDEX,
   BRANCH,
