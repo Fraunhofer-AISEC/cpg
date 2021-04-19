1f8fd810adc397e1b9d3fd20711d3d14fb220f7b
vfsrfs <christian.presa-schnell@aisec.fraunhofer.de>
2021-04-19 10:58:20 +0200

    function declarations and definitions are handled separately from each other

--- src/test/java/de/fraunhofer/aisec/cpg/frontends/cpp/CXXLanguageFrontendTest.java
+++ src/test/java/de/fraunhofer/aisec/cpg/frontends/cpp/CXXLanguageFrontendTest.java
@@ -287,7 +287,7 @@ class CXXLanguageFrontendTest extends BaseTest {
         TestUtils.analyzeAndGetFirstTU(List.of(file), file.getParentFile().toPath(), true);
 
     // should be six function nodes
-    assertEquals(7, declaration.getDeclarations().size());
+    assertEquals(8, declaration.getDeclarations().size());
 
     FunctionDeclaration method = declaration.getDeclarationAs(0, FunctionDeclaration.class);
     assertEquals("function0(int)void", method.getSignature());
