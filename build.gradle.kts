a5091eb14a50045d616c43a406d51f5dd286ba57
Christian Banse <christian.banse@aisec.fraunhofer.de>
2021-04-21 21:20:54 +0200

    Added copyright adding for python files

--- build.gradle.kts
+++ build.gradle.kts
@@ -228,7 +228,8 @@ spotless {
  *                   \${'$'}${'$'}${'$'}${'$'}${'$'}   |${'$'}${'$'} |      \${'$'}${'$'}${'$'}${'$'}${'$'}   |
  *                    \______/ \__|       \______/
  *
- */""").yearSeparator(" - ")
+ */
+""").yearSeparator(" - ")
     }
     kotlin {
         ktfmt().kotlinlangStyle()
@@ -256,10 +257,38 @@ spotless {
  *                   \${'$'}${'$'}${'$'}${'$'}${'$'}   |${'$'}${'$'} |      \${'$'}${'$'}${'$'}${'$'}${'$'}   |
  *                    \______/ \__|       \______/
  *
- */""").yearSeparator(" - ")
+ */
+""").yearSeparator(" - ")
     }
 
-
+    python {
+        target("src/main/**/*.py")
+        licenseHeader("""#
+# Copyright (c) ${"$"}YEAR, Fraunhofer AISEC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#                    ${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}\  ${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}\   ${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}\
+#                   ${'$'}${'$'}  __${'$'}${'$'}\ ${'$'}${'$'}  __${'$'}${'$'}\ ${'$'}${'$'}  __${'$'}${'$'}\
+#                   ${'$'}${'$'} /  \__|${'$'}${'$'} |  ${'$'}${'$'} |${'$'}${'$'} /  \__|
+#                   ${'$'}${'$'} |      ${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}  |${'$'}${'$'} |${'$'}${'$'}${'$'}${'$'}\
+#                   ${'$'}${'$'} |      ${'$'}${'$'}  ____/ ${'$'}${'$'} |\_${'$'}${'$'} |
+#                   ${'$'}${'$'} |  ${'$'}${'$'}\ ${'$'}${'$'} |      ${'$'}${'$'} |  ${'$'}${'$'} |
+#                   \${'$'}${'$'}${'$'}${'$'}${'$'}   |${'$'}${'$'} |      \${'$'}${'$'}${'$'}${'$'}${'$'}   |
+#                    \______/ \__|       \______/
+#
+""", "from").yearSeparator(" - ")
+    }
 }
 
 if (project.hasProperty("experimental")) {
