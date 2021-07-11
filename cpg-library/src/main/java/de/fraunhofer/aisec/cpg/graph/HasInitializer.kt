e3472a64f2a3b44a64edd2b5b3613f6c29b8cbbb
Christian Banse <christian.banse@aisec.fraunhofer.de>
2021-07-11 20:51:34 +0200

    Update HasInitializer.kt

--- cpg-library/src/main/java/de/fraunhofer/aisec/cpg/graph/HasInitializer.kt
+++ cpg-library/src/main/java/de/fraunhofer/aisec/cpg/graph/HasInitializer.kt
@@ -5,6 +5,6 @@ import de.fraunhofer.aisec.cpg.graph.statements.expressions.Expression
 /** Specifies that a certain node has an initializer. */
 interface HasInitializer {
 
-    val initializer: Expression?
+    var initializer: Expression?
 
 }
