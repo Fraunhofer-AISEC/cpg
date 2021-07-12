3b99d1dd49795a082a973ff00fc5c5d61e051389
Banse, Christian <christian.banse@aisec.fraunhofer.de>
2021-07-12 12:13:43 +0200

    Fixed path

--- cpg-library/src/main/nodejs/webpack.config.js
+++ cpg-library/src/main/nodejs/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './parser.js',
+    entry: './src/parser.js',
     target: 'node',
     output: {
         path: path.resolve(__dirname, '../../../build/resources/main/nodejs'),
