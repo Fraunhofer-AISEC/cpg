114b9502713771a86578d55d8f54ae2368096a7e
Maximilian Kaul <maximilian.kaul@aisec.fraunhofer.de>
2021-08-02 16:05:26 +0200

    set lang frontend for typemanager

--- cpg-library/src/main/java/de/fraunhofer/aisec/cpg/frontends/python/PythonLanguageFrontend.kt
+++ cpg-library/src/main/java/de/fraunhofer/aisec/cpg/frontends/python/PythonLanguageFrontend.kt
@@ -29,13 +29,15 @@ import de.fraunhofer.aisec.cpg.ExperimentalPython
 import de.fraunhofer.aisec.cpg.TranslationConfiguration
 import de.fraunhofer.aisec.cpg.frontends.LanguageFrontend
 import de.fraunhofer.aisec.cpg.frontends.TranslationException
+import de.fraunhofer.aisec.cpg.graph.TypeManager
 import de.fraunhofer.aisec.cpg.graph.declarations.TranslationUnitDeclaration
 import de.fraunhofer.aisec.cpg.passes.scopes.ScopeManager
 import de.fraunhofer.aisec.cpg.sarif.PhysicalLocation
 import java.io.File
-import java.lang.Exception
 import java.nio.file.Path
-import jep.*
+import jep.JepConfig
+import jep.JepException
+import jep.SubInterpreter
 
 @ExperimentalPython
 class PythonLanguageFrontend(config: TranslationConfiguration, scopeManager: ScopeManager?) :
@@ -46,6 +48,7 @@ class PythonLanguageFrontend(config: TranslationConfiguration, scopeManager: Sco
 
     @Throws(TranslationException::class)
     override fun parse(file: File): TranslationUnitDeclaration {
+        TypeManager.getInstance().setLanguageFrontend(this)
         return parseInternal(file.readText(Charsets.UTF_8), file.path)
     }
 
