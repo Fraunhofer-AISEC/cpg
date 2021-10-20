9d3612caa6f9e4cbc1930cc226c1f96473b32858
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2021-10-20 08:11:59 +0200

    Convert ConditionalBranchStatement to kotlin file

---
+++ cpg-library/src/main/java/de/fraunhofer/aisec/cpg/graph/statements/ConditionalBranchStatement.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2021, Fraunhofer AISEC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *                    $$$$$$\  $$$$$$$\   $$$$$$\
+ *                   $$  __$$\ $$  __$$\ $$  __$$\
+ *                   $$ /  \__|$$ |  $$ |$$ /  \__|
+ *                   $$ |      $$$$$$$  |$$ |$$$$\
+ *                   $$ |      $$  ____/ $$ |\_$$ |
+ *                   $$ |  $$\ $$ |      $$ |  $$ |
+ *                   \$$$$$   |$$ |      \$$$$$   |
+ *                    \______/ \__|       \______/
+ *
+ */
+package de.fraunhofer.aisec.cpg.graph.statements
+
+import de.fraunhofer.aisec.cpg.graph.statements.expressions.Expression
+import java.util.ArrayList
+
+class ConditionalBranchStatement : Statement() {
+    var defaultTargetLabel: LabelStatement? = null
+    private val conditionalTargets: MutableList<Pair<Expression, LabelStatement>> = ArrayList()
+    fun getConditionalTargets(): List<Pair<Expression, LabelStatement>> {
+        return conditionalTargets
+    }
+
+    fun addConditionalTarget(condition: Expression, label: LabelStatement) {
+        conditionalTargets.add(Pair(condition, label))
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        }
+        if (other !is ConditionalBranchStatement) {
+            return false
+        }
+        val that = other
+        return (super.equals(that)
+                && conditionalTargets == that.conditionalTargets
+                && defaultTargetLabel == that.defaultTargetLabel)
+    }
+
+    override fun hashCode(): Int {
+        return super.hashCode()
+    }
+}
