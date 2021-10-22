a0b3db9b00c3b1372a6e0d5ecac41ba80fc86bd2
Christian Banse <oxisto@aybaze.com>
2021-10-22 20:20:14 +0200

    ++

--- cpg-library/src/main/java/de/fraunhofer/aisec/cpg/graph/TypeManager.java
+++ cpg-library/src/main/java/de/fraunhofer/aisec/cpg/graph/TypeManager.java
@@ -75,7 +75,7 @@ public class TypeManager {
   }
 
   @NonNull
-  private final Map<HasType, Set<Type>> typeCache =
+  private final Map<HasType, List<Type>> typeCache =
       Collections.synchronizedMap(new IdentityHashMap<>());
 
   @NonNull
@@ -286,13 +286,13 @@ public class TypeManager {
     typeSystemActive = active;
   }
 
-  public Map<HasType, Set<Type>> getTypeCache() {
+  public Map<HasType, List<Type>> getTypeCache() {
     return typeCache;
   }
 
   public synchronized void cacheType(HasType node, Type type) {
     if (!isUnknown(type)) {
-      typeCache.computeIfAbsent(node, n -> new HashSet<>()).add(type);
+      typeCache.computeIfAbsent(node, n -> new ArrayList<>()).add(type);
     }
   }
 
