acba48cb0cb8789caedc5b47f0ecc835b3f5da7b
Christian Banse <christian.banse@aisec.fraunhofer.de>
2021-12-08 12:40:25 +0100

    Fixed missing kotlin coroutines (#621)

--- cpg-console/build.gradle.kts
+++ cpg-console/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-script-runtime")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC")
     implementation("org.jline:jline:3.21.0")
 
     implementation("org.jetbrains.kotlinx:ki-shell:0.3.3")
