e2ab3462dd580f7ee03e888681ed63909363f8f3
Christian Banse <christian.banse@aisec.fraunhofer.de>
2021-12-08 18:05:24 +0100

    Restore typescript comment handling (#628)
    Co-authored-by: Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>

--- cpg-library/src/test/resources/typescript/function.ts
+++ cpg-library/src/test/resources/typescript/function.ts
@@ -1,9 +1,14 @@
+/*
+ Block comment on a function
+*/
 function someFunction(): Number {
+    // Comment on a variable
     const i = someOtherFunction("hello");
 
     return i;
 }
 
+// Comment on a Function
 function someOtherFunction(s: String): Number {
     return s.length;
 }
