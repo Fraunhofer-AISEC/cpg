e4614fad6f9390f2220d4720b0aaea374e27ed4a
Christian Banse <christian.banse@aisec.fraunhofer.de>
2021-12-08 17:50:52 +0100

    Revert "Parse comments and locations feature to the typescript frontend (#616)" (#627)
    This reverts commit 64f385c26f3849c16c595a6543f4b598b8177d43.

--- cpg-library/src/test/resources/typescript/function.ts
+++ cpg-library/src/test/resources/typescript/function.ts
@@ -1,14 +1,9 @@
-/*
- Block comment on a function
-*/
 function someFunction(): Number {
-    // Comment on a variable
     const i = someOtherFunction("hello");
 
     return i;
 }
 
-// Comment on a Function
 function someOtherFunction(s: String): Number {
     return s.length;
 }
