2d1a621b905d8cb2d97e5d16d5db89bdb5de5bb9
vfsrfs <christian.presa-schnell@aisec.fraunhofer.de>
2022-01-16 10:17:57 +0100

    handle if statement

--- cpg-core/src/main/java/de/fraunhofer/aisec/cpg/frontends/cpp2/StatementHandler.kt
+++ cpg-core/src/main/java/de/fraunhofer/aisec/cpg/frontends/cpp2/StatementHandler.kt
@@ -28,6 +28,7 @@ package de.fraunhofer.aisec.cpg.frontends.cpp2
 import de.fraunhofer.aisec.cpg.frontends.Handler
 import de.fraunhofer.aisec.cpg.graph.NodeBuilder
 import de.fraunhofer.aisec.cpg.graph.NodeBuilder.newDeclarationStatement
+import de.fraunhofer.aisec.cpg.graph.NodeBuilder.newIfStatement
 import de.fraunhofer.aisec.cpg.graph.NodeBuilder.newReturnStatement
 import de.fraunhofer.aisec.cpg.graph.statements.Statement
 import de.fraunhofer.aisec.cpg.graph.types.ObjectType
@@ -46,6 +47,7 @@ class StatementHandler(lang: CXXLanguageFrontend2) :
             "expression_statement" -> handleExpressionStatement(node)
             "return_statement" -> handleReturnStatement(node)
             "for_range_loop" -> handleForEachStatement(node)
+            "if_statement" -> handleIfStatement(node)
             else -> {
                 log.error("Not handling statement of type {} yet", type)
                 null
@@ -163,5 +165,40 @@ class StatementHandler(lang: CXXLanguageFrontend2) :
         lang.scopeManager.leaveScope(forEachStatement)
 
         return forEachStatement
+    }
+
+    private fun handleIfStatement(node: Node): Statement {
+        val ifStatement = newIfStatement(lang.getCodeFromRawNode(node))
+        lang.scopeManager.enterScope(ifStatement)
+
+        // Handle constexpr
+        val constexpr = node.childByFieldName("constexpr")
+        if (!constexpr.isNull) {
+            ifStatement.isConstExpression = true
+        }
+
+        // Handle Condition
+        val condition = node.childByFieldName("condition")
+        val value = condition.childByFieldName("value")
+        ifStatement.condition = lang.expressionHandler.handle(value)
+
+        // Handle initializer
+        val initializer = condition.childByFieldName("initializer")
+        if (!initializer.isNull) {
+            ifStatement.initializerStatement = handle(initializer)
+        }
+
+        // Handle then branch
+        val thenStatements = node.childByFieldName("consequence")
+        ifStatement.thenStatement = handle(thenStatements)
+
+        // Handle else branch
+        val elseStatement = node.childByFieldName("alternative")
+        if (!elseStatement.isNull) {
+            ifStatement.elseStatement = handle(elseStatement)
+        }
+
+        lang.scopeManager.leaveScope(ifStatement)
+        return ifStatement
     }
 }
