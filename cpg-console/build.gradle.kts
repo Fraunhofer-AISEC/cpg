209213f0ed75a52f485e0318b79393189a845870
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2022-02-16 15:48:38 +0100

    Adding the two publishing sections in both gradle files

--- cpg-console/build.gradle.kts
+++ cpg-console/build.gradle.kts
@@ -25,6 +25,54 @@
  */
 plugins {
     application
+    `maven-publish`
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                artifactId = "cpg-console"
+                name.set("Code Property Graph - Console")
+                description.set("An Application to translate source code into a Code Property Graph and perform different types of analysis on the resulting graph.")
+                url.set("https://github.com/Fraunhofer-AISEC/cpg")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("oxisto")
+                        organization.set("Fraunhofer AISEC")
+                        organizationUrl.set("https://www.aisec.fraunhofer.de")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com:Fraunhofer-AISEC/cpg.git")
+                    developerConnection.set("scm:git:ssh://github.com:Fraunhofer-AISEC/cpg.git")
+                    url.set("https://github.com/Fraunhofer-AISEC/cpg")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+
+            credentials {
+                val mavenCentralUsername: String? by project
+                val mavenCentralPassword: String? by project
+
+                username = mavenCentralUsername
+                password = mavenCentralPassword
+            }
+        }
+    }
 }
 
 application {
--- cpg-neo4j/build.gradle.kts
+++ cpg-neo4j/build.gradle.kts
@@ -25,10 +25,58 @@
  */
 plugins {
     application
+    `maven-publish`
 }
 
 application {
     mainClass.set("de.fraunhofer.aisec.cpg_vis_neo4j.ApplicationKt")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                artifactId = "cpg-neo4j"
+                name.set("Code Property Graph - Neo4j")
+                description.set("An Application to translate and persist specified source code as a Code Property Graph to an installed instance of the Neo4j Graph Database.")
+                url.set("https://github.com/Fraunhofer-AISEC/cpg")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("oxisto")
+                        organization.set("Fraunhofer AISEC")
+                        organizationUrl.set("https://www.aisec.fraunhofer.de")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com:Fraunhofer-AISEC/cpg.git")
+                    developerConnection.set("scm:git:ssh://github.com:Fraunhofer-AISEC/cpg.git")
+                    url.set("https://github.com/Fraunhofer-AISEC/cpg")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+
+            credentials {
+                val mavenCentralUsername: String? by project
+                val mavenCentralPassword: String? by project
+
+                username = mavenCentralUsername
+                password = mavenCentralPassword
+            }
+        }
+    }
 }
 
 tasks.withType<Test> {
