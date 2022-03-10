d3b2eed55e36726ca13bcce511a9b1ed00936162
Christian Banse <oxisto@aybaze.com>
2022-03-10 23:29:03 +0100

    Building multi arch cpgo library on macOS

--- cpg-language-go/src/main/golang/build.sh
+++ cpg-language-go/src/main/golang/build.sh
@@ -15,4 +15,9 @@ fi
 
 export CGO_CFLAGS="-I${JAVA_HOME}/include -I/${JAVA_HOME}/include/${ARCH}"
 
-go build -buildmode=c-shared -o ../resources/libcpgo.${EXTENSION} lib/cpg/main.go
+CGO_ENABLED=1 GOARCH=amd64 go build -buildmode=c-shared -o ../resources/libcpgo-amd64.${EXTENSION} lib/cpg/main.go
+
+if [ $ARCH == "darwin" ]
+then
+CGO_ENABLED=1 GOARCH=arm64 go build -buildmode=c-shared -o ../resources/libcpgo-arm64.${EXTENSION} lib/cpg/main.go
+fi
