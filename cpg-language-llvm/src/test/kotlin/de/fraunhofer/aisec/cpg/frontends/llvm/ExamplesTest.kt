278e8211165018af653aa3754304acda988908fc
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2022-04-06 10:58:29 +0200

    Remove unwanted change

--- cpg-language-llvm/src/test/kotlin/de/fraunhofer/aisec/cpg/frontends/llvm/ExamplesTest.kt
+++ cpg-language-llvm/src/test/kotlin/de/fraunhofer/aisec/cpg/frontends/llvm/ExamplesTest.kt
@@ -53,60 +53,6 @@ class ExamplesTest {
     }
 
     @Test
-    fun testRust2() {
-        val topLevel = Path.of("src", "test", "resources", "llvm", "failed")
-
-        /* Failing:
-                *  - miniz_oxide-bdfdbcfc5f7f7f1b.ll => (4) in ControlFlowSensitiveDFGPass?
-                *  - rustc_demangle-0523e76fb0a24ded.ll => OutOfMemory in ControlFlowSensitiveDFGPass?
-                *  - gimli-42844e93de3eb724.ll => OutOfMemory in ControlFlowSensitiveDFGPass?
-                *  - core-e4cbdb9a079d6d85.ll => OutOfMemory
-                *  - proc_macro-ad55da585703b268.ll -> OutOfMemory
-                *  - std-b98b422506f4d0f3.ll -> OutOfMemory
-                *
-                *
-                * (4) Caused by: java.lang.StackOverflowError
-        at java.base/java.util.Objects.equals(Objects.java:77)
-        at de.fraunhofer.aisec.cpg.graph.statements.expressions.CastExpression.equals(CastExpression.java:135)
-        at java.base/java.util.HashMap$TreeNode.find(HashMap.java:1932)
-        at java.base/java.util.HashMap$TreeNode.find(HashMap.java:1942)
-        at java.base/java.util.HashMap$TreeNode.find(HashMap.java:1942)
-        at java.base/java.util.HashMap$TreeNode.find(HashMap.java:1942)
-        at java.base/java.util.HashMap$TreeNode.find(HashMap.java:1942)
-        at java.base/java.util.HashMap$TreeNode.find(HashMap.java:1942)
-        at java.base/java.util.HashMap$TreeNode.find(HashMap.java:1942)
-        at java.base/java.util.HashMap$TreeNode.getTreeNode(HashMap.java:1954)
-        at java.base/java.util.HashMap.getNode(HashMap.java:572)
-        at java.base/java.util.HashMap.containsKey(HashMap.java:592)
-        at de.fraunhofer.aisec.cpg.passes.ControlFlowSensitiveDFGPass$FunctionLevelFixpointIterator.iterateTillFixpoint(ControlFlowSensitiveDFGPass.java:266)
-        at de.fraunhofer.aisec.cpg.passes.ControlFlowSensitiveDFGPass$FunctionLevelFixpointIterator.iterateTillFixpoint(ControlFlowSensitiveDFGPass.java:303)
-        at de.fraunhofer.aisec.cpg.passes.ControlFlowSensitiveDFGPass$FunctionLevelFixpointIterator.iterateTillFixpoint(ControlFlowSensitiveDFGPass.java:303)
-        at de.fraunhofer.aisec.cpg.passes.ControlFlowSensitiveDFGPass$FunctionLevelFixpointIterator.iterateTillFixpoint(ControlFlowSensitiveDFGPass.java:303)
-        at de.fraunhofer.aisec.cpg.passes.ControlFlowSensitiveDFGPass$FunctionLevelFixpointIterator.iterateTillFixpoint(ControlFlowSensitiveDFGPass.java:303)
-        at de.fraunhofer.aisec.cpg.passes.ControlFlowSensitiveDFGPass$FunctionLevelFixpointIterator.iterateTillFixpoint(ControlFlowSensitiveDFGPass.java:303)
-        at de.fraunhofer.aisec.cpg.passes.ControlFlowSensitiveDFGPass$FunctionLevelFixpointIterator.iterateTillFixpoint(ControlFlowSensitiveDFGPass.java:303)
-        at de.fraunhofer.aisec.cpg.passes.ControlFlowSensitiveDFGPass$FunctionLevelFixpointIterator.iterateTillFixpoint(ControlFlowSensitiveDFGPass.java:303)
-        at de.fraunhofer.aisec.cpg.passes.ControlFlowSensitiveDFGPass$FunctionLevelFixpointIterator.iterateTillFixpoint(ControlFlowSensitiveDFGPass.java:303)
-        at de.fraunhofer.aisec.cpg.passes.ControlFlowSensitiveDFGPass$FunctionLevelFixpointIterator.iterateTillFixpoint(ControlFlowSensitiveDFGPass.java:303)
-        at de.fraunhofer.aisec.cpg.passes.ControlFlowSensitiveDFGPass$FunctionLevelFixpointIterator.iterateTillFixpoint(ControlFlowSensitiveDFGPass.java:303)
-        at de.fraunhofer.aisec.cpg.passes.ControlFlowSensitiveDFGPass$FunctionLevelFixpointIterator.iterateTillFixpoint(ControlFlowSensitiveDFGPass.java:303)
-                */
-        val tu =
-            TestUtils.analyzeAndGetFirstTU(
-                listOf(topLevel.resolve("proc_macro-ad55da585703b268.ll").toFile()),
-                topLevel,
-                true
-            ) {
-                it.registerLanguage(
-                    LLVMIRLanguageFrontend::class.java,
-                    LLVMIRLanguageFrontend.LLVM_EXTENSIONS
-                )
-            }
-
-        assertNotNull(tu)
-    }
-
-    @Test
     fun testIf() {
         val topLevel = Path.of("src", "test", "resources", "llvm", "examples", "llvm")
 
