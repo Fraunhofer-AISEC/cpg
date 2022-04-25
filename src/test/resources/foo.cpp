8b370cc7ceb775b0fb18c6ffac0bed5182d88c97
Christian Banse <oxisto@aybaze.com>
2022-04-25 12:20:51 +0200

    Added missing file

---
+++ src/test/resources/foo.cpp
@@ -0,0 +1,25 @@
+class A {
+public:
+        void foo(int a);
+};
+
+class B {
+public:
+        void foo(int a);
+};
+
+void B::foo(int a) {
+}
+
+int main() {
+        A a;
+        B b;
+
+        a.foo(1);
+        b.foo(2);
+
+        foo();
+
+        C c;
+        c.foo(3);
+}
