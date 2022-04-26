b0ea44be9230d92d724376249e1b6a46aaf276a1
Christian Banse <oxisto@aybaze.com>
2022-04-26 19:51:24 +0200

    Quick alternative using reified function

---
+++ cpg-console/src/main/kotlin/de/fraunhofer/aisec/cpg/query/Query.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022, Fraunhofer AISEC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *                    $$$$$$\  $$$$$$$\   $$$$$$\
+ *                   $$  __$$\ $$  __$$\ $$  __$$\
+ *                   $$ /  \__|$$ |  $$ |$$ /  \__|
+ *                   $$ |      $$$$$$$  |$$ |$$$$\
+ *                   $$ |      $$  ____/ $$ |\_$$ |
+ *                   $$ |  $$\ $$ |      $$ |  $$ |
+ *                   \$$$$$   |$$ |      \$$$$$   |
+ *                    \______/ \__|       \______/
+ *
+ */
+package de.fraunhofer.aisec.cpg.query
+
+import de.fraunhofer.aisec.cpg.ExperimentalGraph
+import de.fraunhofer.aisec.cpg.TranslationResult
+import de.fraunhofer.aisec.cpg.analysis.SizeEvaluator
+import de.fraunhofer.aisec.cpg.graph.Node
+import de.fraunhofer.aisec.cpg.graph.graph
+
+@ExperimentalGraph
+inline fun <reified T : Node> TranslationResult.all(
+    sel: (T) -> Boolean,
+    mustSatisfy: (T) -> Boolean
+): Boolean {
+    var nodes = this.graph.nodes.filterIsInstance<T>()
+
+    // filter the nodes according to the selector
+    nodes = nodes.filter(sel)
+
+    return nodes.all(mustSatisfy)
+}
+
+fun sizeof(n: Node?): Int {
+    val eval = SizeEvaluator()
+    // TODO(oxisto): This cast could potentially go wrong, but if its not an int, its not really a
+    // size
+    return eval.evaluate(n) as? Int ?: -1
+}
---
+++ cpg-console/src/test/kotlin/de/fraunhofer/aisec/cpg/analysis2/Analysis2Test.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022, Fraunhofer AISEC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *                    $$$$$$\  $$$$$$$\   $$$$$$\
+ *                   $$  __$$\ $$  __$$\ $$  __$$\
+ *                   $$ /  \__|$$ |  $$ |$$ /  \__|
+ *                   $$ |      $$$$$$$  |$$ |$$$$\
+ *                   $$ |      $$  ____/ $$ |\_$$ |
+ *                   $$ |  $$\ $$ |      $$ |  $$ |
+ *                   \$$$$$   |$$ |      \$$$$$   |
+ *                    \______/ \__|       \______/
+ *
+ */
+package de.fraunhofer.aisec.cpg.analysis2
+
+import de.fraunhofer.aisec.cpg.ExperimentalGraph
+import de.fraunhofer.aisec.cpg.TranslationConfiguration
+import de.fraunhofer.aisec.cpg.TranslationManager
+import de.fraunhofer.aisec.cpg.graph.statements.expressions.CallExpression
+import de.fraunhofer.aisec.cpg.query.all
+import de.fraunhofer.aisec.cpg.query.sizeof
+import java.io.File
+import kotlin.test.assertFalse
+import org.junit.jupiter.api.Test
+
+class Analysis2Test {
+    @OptIn(ExperimentalGraph::class)
+    @Test
+    fun testMemcpyTooLargeQuery2() {
+        val config =
+            TranslationConfiguration.builder()
+                .sourceLocations(File("src/test/resources/vulnerable.cpp"))
+                .defaultPasses()
+                .defaultLanguages()
+                .build()
+
+        val analyzer = TranslationManager.builder().config(config).build()
+        val result = analyzer.analyze().get()
+
+        val ok =
+            result.all<CallExpression>({ it.name == "memcpy" }) {
+                sizeof(it.arguments[0]) > sizeof(it.arguments[1])
+            }
+
+        assertFalse(ok)
+    }
+}
