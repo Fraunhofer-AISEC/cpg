625646aaf4aeb28b2494dfa519c0ed196eb98476
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2022-05-19 10:31:58 +0200

    Parses single line comments with python internals tokenizer and matches them to the right node in the cpg

--- cpg-language-python/src/main/python/cpg.py
+++ cpg-language-python/src/main/python/cpg.py
@@ -23,6 +23,9 @@
 #                    \______/ \__|       \______/
 #
 from CPGPython import PythonASTToCPG
+import io
+import tokenize
+from de.fraunhofer.aisec.cpg.sarif import Region
 
 
 def parse_code(code, filename, frontend):
@@ -30,7 +33,33 @@ def parse_code(code, filename, frontend):
         converter = PythonASTToCPG(filename, frontend, code)
         converter.execute()
 
-        return converter.tud
+        tud = converter.tud
+
+        parse_comments(code, tud, frontend)
+
+        return tud
     except Exception as e:
         frontend.log.error("Buidling the CPG failed with exception: %s" % (e))
         raise e
+
+def parse_comments(code, tud, frontend):
+        reader = io.StringIO(code).read
+        comment_tokens = (t for t in tokenize.generate_tokens(reader) if t.type == tokenize.COMMENT or t.type == tokenize.NL or t.type == tokenize.NEWLINE)
+        comment = None
+        nr_newlines = 0
+        nl_position = -1
+        cmt_position = 0
+        for token in comment_tokens:
+            print(token)
+            # comment = next(comment_tokens, None)
+            if token.type == tokenize.COMMENT:
+                comment = code[token.start[1]:]
+                cmt_position = token.start[1]
+            if token.type == tokenize.NL or token.type == tokenize.NEWLINE:
+                if comment:
+                    comment = comment[:token.start[1] - (len(code) - len(comment))]
+                    frontend.matchCommentToNode(comment, Region(nr_newlines + 1,cmt_position - nl_position,nr_newlines + 1,token.start[1]-nl_position), tud)
+                    comment = None
+
+                nr_newlines += 1
+                nl_position = token.start[1]
