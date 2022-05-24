a0563c2b2b71adb264866702dceca6e15fe69890
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2022-05-24 14:39:50 +0200

    Fix compiler error, add tests

--- cpg-console/src/test/kotlin/de/fraunhofer/aisec/cpg/analysis2/Analysis2Test.kt
+++ cpg-console/src/test/kotlin/de/fraunhofer/aisec/cpg/analysis2/Analysis2Test.kt
@@ -29,8 +29,12 @@ import de.fraunhofer.aisec.cpg.ExperimentalGraph
 import de.fraunhofer.aisec.cpg.TranslationConfiguration
 import de.fraunhofer.aisec.cpg.TranslationManager
 import de.fraunhofer.aisec.cpg.graph.Assignment
+import de.fraunhofer.aisec.cpg.graph.declarations.VariableDeclaration
+import de.fraunhofer.aisec.cpg.graph.statements.expressions.ArrayCreationExpression
+import de.fraunhofer.aisec.cpg.graph.statements.expressions.ArraySubscriptionExpression
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.CallExpression
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.DeclaredReferenceExpression
+import de.fraunhofer.aisec.cpg.passes.EdgeCachePass
 import de.fraunhofer.aisec.cpg.passes.followNextEOG
 import de.fraunhofer.aisec.cpg.query.*
 import java.io.File
@@ -54,7 +58,7 @@ class Analysis2Test {
 
         val (ok, fails) =
             result.all<CallExpression>({ it.name == "memcpy" }) {
-                sizeof(it.arguments[0]) > sizeof(it.arguments[1]) && 1 > 25
+                sizeof(it.arguments[0]) > sizeof(it.arguments[1])
             }
 
         assertFalse(ok)
@@ -82,6 +86,32 @@ class Analysis2Test {
     }
 
     @Test
+    fun testDoubleFree() {
+        val config =
+            TranslationConfiguration.builder()
+                .sourceLocations(File("src/test/resources/vulnerable.cpp"))
+                .defaultPasses()
+                .defaultLanguages()
+                .build()
+
+        val analyzer = TranslationManager.builder().config(config).build()
+        val result = analyzer.analyze().get()
+
+        val (ok, fails) =
+            result.all<CallExpression>({ it.name == "free" }) { outer ->
+                val path =
+                    outer.followNextEOG {
+                        (it.end as? DeclaredReferenceExpression)?.refersTo ==
+                            (outer.arguments[0] as? DeclaredReferenceExpression)?.refersTo
+                    }
+                return@all path?.isEmpty() == true
+            }
+
+        assertFalse(ok)
+        println(fails)
+    }
+
+    @Test
     fun testParameterEqualsConst() {
         val config =
             TranslationConfiguration.builder()
@@ -94,19 +124,7 @@ class Analysis2Test {
         val result = analyzer.analyze().get()
 
         val (ok, _) =
-            result.all<CallExpression>({ it.name == "free" }) { outer ->
-                val path =
-                    outer.followNextEOG {
-                        (it.end as? DeclaredReferenceExpression)?.refersTo ==
-                            (outer.arguments[0] as? DeclaredReferenceExpression)?.refersTo
-                    }
-                return@all path?.isEmpty() == true
-            }
-
-        assertTrue(ok)
-
-        // (ok, _) = result.all<CallExpression>({ it.name == "memcpy" }) { it.arguments[2].intValue
-        // == 11 }
+            result.all<CallExpression>({ it.name == "memcpy" }) { it.arguments[2].intValue == 11 }
 
         assertTrue(ok)
     }
@@ -123,8 +141,94 @@ class Analysis2Test {
         val analyzer = TranslationManager.builder().config(config).build()
         val result = analyzer.analyze().get()
 
-        val ok = result.all<Assignment> { it.value() < const(5) }
+        val (ok, _) = result.all<Assignment> { it.value() < const(5) }
 
         assertTrue(ok)
+    }
+
+    @Test
+    fun testOutOfBoundsQuery() {
+        val config =
+            TranslationConfiguration.builder()
+                .sourceLocations(File("src/test/resources/array.cpp"))
+                .defaultPasses()
+                .defaultLanguages()
+                .build()
+
+        val analyzer = TranslationManager.builder().config(config).build()
+        val result = analyzer.analyze().get()
+
+        val (ok, fails) =
+            result.all<ArraySubscriptionExpression> {
+                max(it.subscriptExpression) <
+                    min(
+                        (((it.arrayExpression as DeclaredReferenceExpression).refersTo as
+                                    VariableDeclaration)
+                                .initializer as
+                                ArrayCreationExpression)
+                            .dimensions[0]
+                    ) && min(it.subscriptExpression) >= 0
+            }
+        assertFalse(ok)
+        println(fails)
+    }
+
+    @Test
+    fun testOutOfBoundsQuery2() {
+        val config =
+            TranslationConfiguration.builder()
+                .sourceLocations(File("src/test/resources/array2.cpp"))
+                .defaultPasses()
+                .defaultLanguages()
+                .registerPass(EdgeCachePass())
+                .build()
+
+        val analyzer = TranslationManager.builder().config(config).build()
+        val result = analyzer.analyze().get()
+
+        val (ok, fails) =
+            result.all<ArraySubscriptionExpression> {
+                max(it.subscriptExpression) <
+                    min(
+                        (((it.arrayExpression as DeclaredReferenceExpression).refersTo as
+                                    VariableDeclaration)
+                                .initializer as
+                                ArrayCreationExpression)
+                            .dimensions[0]
+                    ) && min(it.subscriptExpression) >= 0
+            }
+        assertFalse(ok)
+        println(fails)
+    }
+
+    @Test
+    fun testOutOfBoundsQueryCorrect() {
+        val config =
+            TranslationConfiguration.builder()
+                .sourceLocations(File("src/test/resources/array_correct.cpp"))
+                .defaultPasses()
+                .defaultLanguages()
+                .registerPass(EdgeCachePass())
+                .build()
+
+        val analyzer = TranslationManager.builder().config(config).build()
+        val result = analyzer.analyze().get()
+
+        val (ok, fails) =
+            result.all<ArraySubscriptionExpression> {
+                val max_sub = max(it.subscriptExpression)
+                val min_dim =
+                    min(
+                        (((it.arrayExpression as DeclaredReferenceExpression).refersTo as
+                                    VariableDeclaration)
+                                .initializer as
+                                ArrayCreationExpression)
+                            .dimensions[0]
+                    )
+                val min_sub = min(it.subscriptExpression)
+                return@all max_sub < min_dim && min_sub >= 0
+            }
+        assertTrue(ok)
+        println(fails)
     }
 }
--- cpg-console/src/test/resources/vulnerable.cpp
+++ cpg-console/src/test/resources/vulnerable.cpp
@@ -2,4 +2,6 @@ int main() {
     char array[6] = "hello";
     memcpy(array, "Hello world", 11);
     printf(array);
+    free(array);
+    free(array);
 }
