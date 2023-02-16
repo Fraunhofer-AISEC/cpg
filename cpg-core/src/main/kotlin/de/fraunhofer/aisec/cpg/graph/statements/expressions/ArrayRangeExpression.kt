e0c38134eb0717c621f21279fcffc658318b0db1
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2023-02-16 17:12:40 +0100

    Add ArrayRangeExpressions to the EOGPass

--- cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/graph/statements/expressions/ArrayRangeExpression.kt
+++ cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/graph/statements/expressions/ArrayRangeExpression.kt
@@ -26,18 +26,24 @@
 package de.fraunhofer.aisec.cpg.graph.statements.expressions
 
 import de.fraunhofer.aisec.cpg.graph.SubGraph
+import de.fraunhofer.aisec.cpg.graph.newLiteral
 import java.util.Objects
 
 /** Expressions of the form `floor ... ceiling` */
 class ArrayRangeExpression : Expression() {
     @field:SubGraph("AST") var floor: Expression? = null
 
+    @field:SubGraph("AST") var step: Expression? = newLiteral(1)
+
     @field:SubGraph("AST") var ceiling: Expression? = null
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is ArrayRangeExpression) return false
-        return super.equals(other) && floor == other.floor && ceiling == other.ceiling
+        return super.equals(other) &&
+            floor == other.floor &&
+            ceiling == other.ceiling &&
+            step == other.step
     }
 
     override fun hashCode() = Objects.hash(super.hashCode(), floor, ceiling)
