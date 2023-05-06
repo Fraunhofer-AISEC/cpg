a25e68756b14736136dc87706e5d44b3b78f79fe
Christian Banse <oxisto@aybaze.com>
2023-05-06 10:06:54 +0200

    Removing plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,17 +2,6 @@ rootProject.name = "cpg"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-plugins {
-    id("com.gradle.enterprise") version("3.13.1")
-}
-
-gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-    }
-}
-
 include(":cpg-all")
 include(":cpg-core")
 include(":cpg-analysis")
