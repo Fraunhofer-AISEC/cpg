4f8c8949b3345975ce49caf790a181d769369a25
Christian Banse <christian.banse@aisec.fraunhofer.de>
2023-11-24 12:42:25 +0100

    Rebased with parallel passes

--- cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/ControlFlowSensitiveDFGPass.kt
+++ cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/ControlFlowSensitiveDFGPass.kt
@@ -111,13 +111,17 @@ open class ControlFlowSensitiveDFGPass(ctx: TranslationContext) : EOGStarterPass
                 // elements. We have the indices here, so it's amazing.
                 key.elements.forEachIndexed { i, element ->
                     element.addAllPrevDFG(
-                        value.elements.filterNot { it is VariableDeclaration && key == it },
+                        value.elements.filterNot {
+                            (it is VariableDeclaration || it is ParameterDeclaration) && key == it
+                        },
                         mutableMapOf(Properties.INDEX to i)
                     )
                 }
             } else {
                 key.addAllPrevDFG(
-                    value.elements.filterNot { it is VariableDeclaration && key == it }
+                    value.elements.filterNot {
+                        (it is VariableDeclaration || it is ParameterDeclaration) && key == it
+                    }
                 )
             }
         }
