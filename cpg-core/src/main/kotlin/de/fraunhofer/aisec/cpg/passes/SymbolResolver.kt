f0e2e4a30b74495279fb346e6631855ea9855c79
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2023-11-27 13:56:08 +0100

    More simple signature check

--- cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/SymbolResolver.kt
+++ cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/SymbolResolver.kt
@@ -852,23 +852,6 @@ open class SymbolResolver(ctx: TranslationContext) : ComponentPass(ctx) {
     }
 
     /**
-     * @param signature of the ConstructExpression
-     * @param recordDeclaration matching the class the ConstructExpression wants to construct
-     * @return ConstructorDeclaration that matches the provided signature
-     */
-    protected fun getConstructorDeclarationDirectMatch(
-        signature: List<Type>,
-        recordDeclaration: RecordDeclaration
-    ): ConstructorDeclaration? {
-        for (constructor in recordDeclaration.constructors) {
-            if (constructor.hasSignature(signature)) {
-                return constructor
-            }
-        }
-        return null
-    }
-
-    /**
      * @param constructExpression we want to find an invocation target for
      * @param recordDeclaration associated with the Object the ConstructExpression constructs
      * @return a ConstructDeclaration that is an invocation of the given ConstructExpression. If
@@ -881,7 +864,8 @@ open class SymbolResolver(ctx: TranslationContext) : ComponentPass(ctx) {
     ): ConstructorDeclaration {
         val signature = constructExpression.signature
         var constructorCandidate =
-            getConstructorDeclarationDirectMatch(signature, recordDeclaration)
+            recordDeclaration.constructors.firstOrNull { it.hasSignature(signature) }
+
         if (constructorCandidate == null && constructExpression.language is HasDefaultArguments) {
             // Check for usage of default args
             constructorCandidate =
