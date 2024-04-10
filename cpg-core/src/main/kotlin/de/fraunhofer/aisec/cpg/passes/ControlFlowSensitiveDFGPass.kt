19d39832c5ce87dd56738b13163bb171936f4540
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2024-04-10 13:17:45 +0200

    Consider definitions of FunctionDeclarations if there is no body

--- cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/ControlFlowSensitiveDFGPass.kt
+++ cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/ControlFlowSensitiveDFGPass.kt
@@ -78,7 +78,7 @@ open class ControlFlowSensitiveDFGPass(ctx: TranslationContext) : EOGStarterPass
 
         // Calculate the complexity of the function and see, if it exceeds our threshold
         val max = passConfig<Configuration>()?.maxComplexity
-        val c = node.body?.cyclomaticComplexity ?: 0
+        val c = node.definition?.body?.cyclomaticComplexity ?: 0
         if (max != null && c > max) {
             log.info(
                 "Ignoring function ${node.name} because its complexity (${c}) is greater than the configured maximum (${max})"
