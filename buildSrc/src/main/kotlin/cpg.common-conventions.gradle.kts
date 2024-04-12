d4add8aa6edee57f85bcc4d02fe9814fcb3c1b14
Christian Banse <oxisto@clouditor.io>
2024-04-12 21:56:03 +0200

    Using `io.joern:eclipse-cdt-core` release of CDT
    It seems that joern provides releases of CDT on maven central. We could use this instead of having this rather ugly workaround with the ivy repository, which we need to change with every CDT release.
    
    I am not sure how often they will update it. Any opinions on this?

--- buildSrc/src/main/kotlin/cpg.common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/cpg.common-conventions.gradle.kts
@@ -21,16 +21,6 @@ java {
 //
 repositories {
     mavenCentral()
-
-    ivy {
-        setUrl("https://download.eclipse.org/tools/cdt/releases/11.3/cdt-11.3.1/plugins")
-        metadataSources {
-            artifact()
-        }
-        patternLayout {
-            artifact("/[organisation].[module]_[revision].[ext]")
-        }
-    }
 }
 
 //
