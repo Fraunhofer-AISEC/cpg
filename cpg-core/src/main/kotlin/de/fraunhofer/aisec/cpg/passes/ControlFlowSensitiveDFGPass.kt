101f61729b97edd02fbecfcd7480e02d5d65ef30
Mathias Morbitzer <m.morbitzer@runbox.com>
2024-07-22 18:07:23 +0200

    Remove unneeded DFG-Edges for pointer access

--- cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/ControlFlowSensitiveDFGPass.kt
+++ cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/ControlFlowSensitiveDFGPass.kt
@@ -136,7 +136,8 @@ open class ControlFlowSensitiveDFGPass(ctx: TranslationContext) : EOGStarterPass
                         )
                     } else {
                         // In case the previous node is a *-UnaryOperator and its prevDFG is the
-                        // same as it, this should be not a full DFG, but a pointerDataFlow
+                        // same as it, this should be not a full DFG, but a pointerDataFlow.
+                        // Additionally, we can tear down the original DFG edges
                         val prevUnaryOps =
                             key.prevDFG.filter { it is UnaryOperator && it.operatorCode == "*" }
                         if (
@@ -144,6 +145,8 @@ open class ControlFlowSensitiveDFGPass(ctx: TranslationContext) : EOGStarterPass
                                 prevUnaryOps.first().prevDFG.size == 1 &&
                                 prevUnaryOps.first().prevDFG.first() == it
                         ) {
+                            key.removePrevDFG(prevUnaryOps.first())
+                            prevUnaryOps.first().removePrevDFG(it)
                             key.addPrevDFG(it, PointerDataflowGranularity())
                         } else {
                             key.addPrevDFG(it)
