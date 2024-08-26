c6d3abbe13d6e3b53fded9782ab4c3dc9889a138
Robert Haimerl <robert.haimerl@aisec.fraunhofer.de>
2024-08-26 10:42:51 +0200

    update the evaluator name in tests

--- cpg-analysis/src/test/kotlin/de/fraunhofer/aisec/cpg/analysis/SizeEvaluatorTest.kt
+++ cpg-analysis/src/test/kotlin/de/fraunhofer/aisec/cpg/analysis/SizeEvaluatorTest.kt
@@ -25,8 +25,8 @@
  */
 package de.fraunhofer.aisec.cpg.analysis
 
-import de.fraunhofer.aisec.cpg.analysis.collectioneval.CollectionSizeEvaluator
-import de.fraunhofer.aisec.cpg.analysis.collectioneval.LatticeInterval
+import de.fraunhofer.aisec.cpg.analysis.abstracteval.AbstractEvaluator
+import de.fraunhofer.aisec.cpg.analysis.abstracteval.LatticeInterval
 import de.fraunhofer.aisec.cpg.graph.*
 import de.fraunhofer.aisec.cpg.graph.bodyOrNull
 import de.fraunhofer.aisec.cpg.graph.declarations.TranslationUnitDeclaration
@@ -132,7 +132,7 @@ class SizeEvaluatorTest {
         val printArg = printCall.arguments.first()
         assertNotNull(printArg)
 
-        val evaluator = CollectionSizeEvaluator()
+        val evaluator = AbstractEvaluator()
         val value = evaluator.evaluate(printArg)
         assertEquals(LatticeInterval.Bounded(0, 2), value)
     }
