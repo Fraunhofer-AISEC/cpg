ebd3f8798579ef0f0e99c18fc34f67ec51c79d2d
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2024-11-05 16:36:39 +0100

    MapLattice test

--- cpg-core/src/test/kotlin/de/fraunhofer/aisec/cpg/helpers/BasicLatticesTest.kt
+++ cpg-core/src/test/kotlin/de/fraunhofer/aisec/cpg/helpers/BasicLatticesTest.kt
@@ -26,8 +26,10 @@
 package de.fraunhofer.aisec.cpg.helpers
 
 import de.fraunhofer.aisec.cpg.graph.Node
+import de.fraunhofer.aisec.cpg.helpers.functional.MapLattice
 import de.fraunhofer.aisec.cpg.helpers.functional.PowersetLattice
 import de.fraunhofer.aisec.cpg.helpers.functional.PowersetLatticeT
+import de.fraunhofer.aisec.cpg.helpers.functional.emptyMapLattice
 import de.fraunhofer.aisec.cpg.helpers.functional.emptyPowersetLattice
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -105,5 +107,103 @@ class BasicLatticesTest {
         assertNotEquals(blaBlubLattice, blaFooBlub)
         assertEquals(-1, blaBlubLattice.compareTo(blaFooBlub))
         assertEquals(setOf("bla", "blub", "foo"), blaFooBlub.elements)
+    }
+
+    @Test
+    fun testMapLattice() {
+        val emptyLattice1 = emptyMapLattice<String, Set<String>>()
+        val emptyLattice2 = emptyMapLattice<String, Set<String>>()
+        assertEquals(0, emptyLattice1.compareTo(emptyLattice2))
+        assertEquals(emptyLattice1, emptyLattice2)
+
+        val aBlaLattice1 =
+            MapLattice<String, Set<String>>(mapOf("a" to PowersetLattice(setOf("bla"))))
+        val bBlaLattice2 =
+            MapLattice<String, Set<String>>(mapOf("a" to PowersetLattice(setOf("bla"))))
+        assertEquals(0, aBlaLattice1.compareTo(bBlaLattice2))
+        assertEquals(aBlaLattice1, bBlaLattice2)
+        assertNotSame(aBlaLattice1, bBlaLattice2)
+
+        val aBlaFooLattice =
+            MapLattice<String, Set<String>>(mapOf("a" to PowersetLattice(setOf("bla", "foo"))))
+        assertEquals(1, aBlaFooLattice.compareTo(aBlaLattice1))
+        assertNotEquals(aBlaFooLattice, aBlaLattice1)
+        assertEquals(-1, aBlaLattice1.compareTo(aBlaFooLattice))
+        assertNotEquals(aBlaLattice1, aBlaFooLattice)
+
+        val aBlaBFooLattice =
+            MapLattice<String, Set<String>>(
+                mapOf("a" to PowersetLattice(setOf("bla")), "b" to PowersetLattice(setOf("foo")))
+            )
+        assertEquals(1, aBlaBFooLattice.compareTo(aBlaLattice1))
+        assertNotEquals(aBlaBFooLattice, aBlaLattice1)
+        assertEquals(-1, aBlaLattice1.compareTo(aBlaBFooLattice))
+        assertNotEquals(aBlaLattice1, aBlaBFooLattice)
+
+        // Duplicates are equal but not identical. Same for the elements.
+        val emptyDuplicate = emptyLattice1.duplicate()
+        assertNotSame(emptyLattice1, emptyDuplicate)
+        // assertNotSame(emptyLattice1.elements, emptyDuplicate.elements)
+        assertEquals(emptyLattice1, emptyDuplicate)
+        val aBlaLatticeDuplicate = aBlaLattice1.duplicate()
+        assertNotSame(aBlaLattice1, aBlaLatticeDuplicate)
+        assertNotSame(aBlaLattice1.elements, aBlaLatticeDuplicate.elements)
+        assertEquals(aBlaLattice1, aBlaLatticeDuplicate)
+        val aBlaFooLatticeDuplicate = aBlaFooLattice.duplicate()
+        assertNotSame(aBlaFooLattice, aBlaFooLatticeDuplicate)
+        assertNotSame(aBlaBFooLattice.elements, aBlaFooLatticeDuplicate.elements)
+        assertEquals(aBlaFooLattice, aBlaFooLatticeDuplicate)
+        val aBlaBFooLatticeDuplicate = aBlaBFooLattice.duplicate()
+        assertNotSame(aBlaFooLattice, aBlaBFooLatticeDuplicate)
+        assertNotSame(aBlaBFooLattice.elements, aBlaBFooLatticeDuplicate.elements)
+        assertEquals(aBlaBFooLattice, aBlaBFooLatticeDuplicate)
+
+        val emptyLubEmpty = emptyLattice1.lub(emptyLattice1)
+        assertIs<MapLattice<String, Set<String>>>(emptyLubEmpty)
+        assertNotSame(emptyLattice1, emptyLubEmpty)
+        assertEquals(emptyLattice1, emptyLubEmpty)
+        assertEquals(0, emptyLattice1.compareTo(emptyLubEmpty))
+        assertNotSame(emptyLattice2, emptyLubEmpty)
+        assertEquals(emptyLattice1, emptyLubEmpty)
+        assertEquals(0, emptyLattice2.compareTo(emptyLubEmpty))
+
+        val emptyLubABla = emptyLattice1.lub(aBlaLattice1)
+        assertIs<MapLattice<String, Set<String>>>(emptyLubABla)
+        assertNotSame(emptyLattice1, emptyLubABla)
+        assertNotEquals(emptyLattice1, emptyLubABla)
+        assertEquals(-1, emptyLattice1.compareTo(emptyLubABla))
+        assertNotSame(aBlaLattice1, emptyLubABla)
+        assertEquals(aBlaLattice1, emptyLubABla)
+        assertEquals(0, aBlaLattice1.compareTo(emptyLubABla))
+
+        val aFooBBlaLattice =
+            MapLattice<String, Set<String>>(
+                mapOf("a" to PowersetLattice(setOf("foo")), "b" to PowersetLattice(setOf("bla")))
+            )
+        val aBlaFooBBla = aBlaFooLattice.lub(aFooBBlaLattice) // a to {"foo", "bla"}, b to {"bla"}
+        assertIs<MapLattice<String, Set<String>>>(aBlaFooBBla)
+        assertNotSame(emptyLattice1, aBlaFooBBla)
+        assertNotEquals(emptyLattice1, aBlaFooBBla)
+        assertEquals(-1, emptyLattice1.compareTo(aBlaFooBBla))
+        assertEquals(1, aBlaFooBBla.compareTo(emptyLattice1))
+        assertNotSame(aBlaLattice1, aBlaFooBBla)
+        assertNotEquals(aBlaLattice1, aBlaFooBBla)
+        assertEquals(-1, aBlaLattice1.compareTo(aBlaFooBBla))
+        assertEquals(1, aBlaFooBBla.compareTo(aBlaLattice1))
+        assertNotSame(aBlaFooLattice, aBlaFooBBla)
+        assertNotEquals(aBlaFooLattice, aBlaFooBBla)
+        assertEquals(-1, aBlaFooLattice.compareTo(aBlaFooBBla))
+        assertEquals(1, aBlaFooBBla.compareTo(aBlaFooLattice))
+        assertNotSame(aBlaBFooLattice, aBlaFooBBla)
+        assertNotEquals(aBlaBFooLattice, aBlaFooBBla)
+        assertEquals(-1, aBlaBFooLattice.compareTo(aBlaFooBBla))
+        assertEquals(-1, aBlaFooBBla.compareTo(aBlaBFooLattice))
+        assertNotSame(aFooBBlaLattice, aBlaFooBBla)
+        assertNotEquals(aFooBBlaLattice, aBlaFooBBla)
+        assertEquals(-1, aFooBBlaLattice.compareTo(aBlaFooBBla))
+        assertEquals(1, aBlaFooBBla.compareTo(aFooBBlaLattice))
+        assertEquals(setOf("a", "b"), aBlaFooBBla.elements.keys)
+        assertEquals(setOf("bla", "foo"), aBlaFooBBla.elements["a"]?.elements)
+        assertEquals(setOf("bla"), aBlaFooBBla.elements["b"]?.elements)
     }
 }
