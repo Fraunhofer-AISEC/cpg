fcca597bb3ad8e3cc4d4afdc805750ca60cdcbb0
Leutrim Shala <Leutrim.shala@aisec.fraunhofer.de>
2024-11-08 13:54:10 +0100

    Revert changes from other branch

--- cpg-core/src/testFixtures/kotlin/de/fraunhofer/aisec/cpg/test/TestUtils.kt
+++ cpg-core/src/testFixtures/kotlin/de/fraunhofer/aisec/cpg/test/TestUtils.kt
@@ -317,7 +317,7 @@ fun <T : Any?> assertLiteralValue(expected: T, expr: Expression?, message: Strin
     assertEquals(expected, assertIs<Literal<T>>(expr).value, message)
 }
 
-fun ContextProvider.assertResolvedType(fqn: String): Type {
-    var type = ctx?.typeManager?.lookupResolvedType(fqn)
+fun ContextProvider.assertResolvedType(fqn: String, generics: List<Type>? = null): Type {
+    var type = ctx?.typeManager?.lookupResolvedType(fqn, generics)
     return assertNotNull(type)
 }
