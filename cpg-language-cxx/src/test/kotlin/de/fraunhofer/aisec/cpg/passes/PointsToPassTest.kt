9514d44c70a4810cf6702ddea61d19f08af70bc5
Mathias Morbitzer <m.morbitzer@runbox.com>
2024-11-20 15:58:10 +0100

    Added PointsToPassTest

--- cpg-language-cxx/src/test/kotlin/de/fraunhofer/aisec/cpg/passes/PointsToPassTest.kt
+++ cpg-language-cxx/src/test/kotlin/de/fraunhofer/aisec/cpg/passes/PointsToPassTest.kt
@@ -25,102 +25,167 @@
  */
 package de.fraunhofer.aisec.cpg.passes
 
+import de.fraunhofer.aisec.cpg.frontends.cxx.CPPLanguage
+import de.fraunhofer.aisec.cpg.graph.allChildren
 import de.fraunhofer.aisec.cpg.graph.declarations.VariableDeclaration
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.*
+import de.fraunhofer.aisec.cpg.test.analyzeAndGetFirstTU
+import de.fraunhofer.aisec.cpg.test.assertLocalName
 import java.io.File
+import kotlin.io.path.Path
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 class PointsToPassTest {
+    companion object {
+        private val topLevel = java.nio.file.Path.of("src", "test", "resources")
+    }
 
     @Test
-    fun testPointsToPass() {
-        val result =
-            /*analyze(listOf(Path.of(topLevel.toString(), "calls.cpp").toFile()), topLevel, true) {
-                    it.registerLanguage<CPPLanguage>()
-                    it.inferenceConfiguration(
-                        InferenceConfiguration.builder().inferRecords(false).build()
-                    )
-                }
-            val tu = result.components.flatMap { it.translationUnits }.firstOrNull()
-                analyze(File("cpg-language-cxx/src/test/resources/c/pointsto.cpp"))*/
-            translate(File("cpg-language-cxx/src/test/resources/c/pointsto.cpp"))
-                .components
-                .firstOrNull()
-                ?.translationUnits
-                ?.firstOrNull()
-        assertNotNull(result)
+    fun testBasics() {
+        val file = File("src/test/resources/pointsto.cpp")
+        val tu =
+            analyzeAndGetFirstTU(listOf(file), file.parentFile.toPath(), true) {
+                it.registerLanguage<CPPLanguage>()
+                it.registerPass<PointsToPass>()
+            }
+        assertNotNull(tu)
 
+        // Declarations
         val iDecl =
-            result.allChildren<VariableDeclaration> { it.location?.region?.startLine == 4 }.first()
-        val literal0 =
-            result.allChildren<Literal<*>> { it.location?.region?.startLine == 4 }.first()
+            tu.allChildren<VariableDeclaration> { it.location?.region?.startLine == 4 }.first()
         val jDecl =
-            result.allChildren<VariableDeclaration> { it.location?.region?.startLine == 5 }.first()
-        val literal1 =
-            result.allChildren<Literal<*>> { it.location?.region?.startLine == 5 }.first()
+            tu.allChildren<VariableDeclaration> { it.location?.region?.startLine == 5 }.first()
         val aDecl =
-            result.allChildren<VariableDeclaration> { it.location?.region?.startLine == 6 }.first()
+            tu.allChildren<VariableDeclaration> { it.location?.region?.startLine == 6 }.first()
+        val bDecl =
+            tu.allChildren<VariableDeclaration> { it.location?.region?.startLine == 7 }.first()
 
+        // Literals
+        val literal0 = tu.allChildren<Literal<*>> { it.location?.region?.startLine == 4 }.first()
+        val literal1 = tu.allChildren<Literal<*>> { it.location?.region?.startLine == 5 }.first()
+        val literal2 = tu.allChildren<Literal<*>> { it.location?.region?.startLine == 9 }.first()
+        val literal3 = tu.allChildren<Literal<*>> { it.location?.region?.startLine == 17 }.first()
+
+        // PointerReferences
         val iPointerRef =
-            result.allChildren<PointerReference> { it.location?.region?.startLine == 6 }.first()
+            tu.allChildren<PointerReference> { it.location?.region?.startLine == 6 }.first()
+        val jPointerRef =
+            tu.allChildren<PointerReference> { it.location?.region?.startLine == 15 }.first()
 
-        val iRefLine8 =
-            result.allChildren<Reference> { it.location?.region?.startLine == 8 }.first()
-        val iRefLine9 =
-            result.allChildren<Reference> { it.location?.region?.startLine == 9 }.first()
-        val literal2 =
-            result.allChildren<Literal<*>> { it.location?.region?.startLine == 9 }.first()
-        val iRefLine10 =
-            result.allChildren<Reference> { it.location?.region?.startLine == 10 }.first()
-        val iRefLine11 =
-            result.allChildren<Reference> { it.location?.region?.startLine == 11 }.first()
-
+        // PointerDeReferences
         val aPointerDerefLine12 =
-            result.allChildren<PointerDereference> { it.location?.region?.startLine == 12 }.first()
-        val aPointerDerefLine15 =
-            result.allChildren<PointerDereference> { it.location?.region?.startLine == 15 }.first()
+            tu.allChildren<PointerDereference> { it.location?.region?.startLine == 12 }.first()
+        val aPointerDerefLine14 =
+            tu.allChildren<PointerDereference> { it.location?.region?.startLine == 14 }.first()
+        val aPointerDerefLine16 =
+            tu.allChildren<PointerDereference> { it.location?.region?.startLine == 16 }.first()
+        val aPointerDerefLine17 =
+            tu.allChildren<PointerDereference> { it.location?.region?.startLine == 17 }.first()
+        val bPointerDerefLine18 =
+            tu.allChildren<PointerDereference> { it.location?.region?.startLine == 18 }.first()
 
-        assertTrue(iDecl.memoryAddress.name.localName == "i")
-        assertTrue(iDecl.memoryValue.size == 1)
-        assertTrue(iDecl.memoryValue.first() == literal0)
+        // References
+        val iRefLine8 = tu.allChildren<Reference> { it.location?.region?.startLine == 8 }.first()
+        val iRefLine9 = tu.allChildren<Reference> { it.location?.region?.startLine == 9 }.first()
+        val iRefLine10 = tu.allChildren<Reference> { it.location?.region?.startLine == 10 }.first()
+        val iRefLine11 = tu.allChildren<Reference> { it.location?.region?.startLine == 11 }.first()
+        val aRefLine15 =
+            tu.allChildren<Reference> {
+                    it.location?.region?.startLine == 15 && it.name.localName == "a"
+                }
+                .first()
 
-        assertTrue(jDecl.memoryAddress.name.localName == "j")
-        assertTrue(jDecl.memoryValue.size == 1)
-        assertTrue(jDecl.memoryValue.first() == literal1)
+        // UnaryOperators
+        val iUO = tu.allChildren<UnaryOperator> { it.location?.region?.startLine == 13 }.first()
 
-        assertTrue(aDecl.memoryAddress.name.localName == "a")
-        assertTrue(aDecl.memoryValue.size == 1)
-        assertTrue(aDecl.memoryValue.first() == iDecl.memoryAddress)
+        // Line 4
+        assertLocalName("i", iDecl.memoryAddress)
+        assertEquals(1, iDecl.memoryValue.size)
+        assertEquals(literal0, iDecl.memoryValue.first())
 
+        // Line 5
+        assertLocalName("j", jDecl.memoryAddress)
+        assertEquals(1, jDecl.memoryValue.size)
+        assertEquals(literal1, jDecl.memoryValue.first())
+
+        // Line 6
+        assertLocalName("a", aDecl.memoryAddress)
+        assertEquals(1, aDecl.memoryValue.size)
+        assertEquals(aDecl.memoryValue.first(), iDecl.memoryAddress)
         assertTrue(iPointerRef.memoryAddress.isEmpty())
-        assertTrue(iPointerRef.memoryValue.size == 1)
-        assertTrue(iPointerRef.memoryValue.first() == iDecl.memoryAddress)
+        assertEquals(1, iPointerRef.memoryValue.size)
+        assertEquals(iPointerRef.memoryValue.first(), iDecl.memoryAddress)
 
-        assertTrue(iRefLine8.memoryAddress.size == 1)
-        assertTrue(iRefLine8.memoryAddress.first() == iDecl.memoryAddress)
-        assertTrue(iRefLine8.memoryValue.size == 1)
-        assertTrue(iRefLine8.memoryValue.first() == literal0)
+        // Line 7
+        assertLocalName("b", bDecl.memoryAddress)
+        assertEquals(1, bDecl.memoryValue.size)
+        assertEquals(iDecl.memoryAddress, bDecl.memoryValue.first())
 
-        assertTrue(iRefLine9.memoryAddress.size == 1)
-        assertTrue(iRefLine9.memoryAddress.first() == iDecl.memoryAddress)
-        // TODO: fix this assertTrue(iRefLine9.memoryValue.size == 1)
-        assertTrue(iRefLine9.memoryValue.filterIsInstance<Literal<*>>().first() == literal2)
+        // Line 8
+        assertEquals(1, iRefLine8.memoryAddress.size)
+        assertEquals(iDecl.memoryAddress, iRefLine8.memoryAddress.first())
+        assertEquals(1, iRefLine8.memoryValue.size)
+        assertEquals(literal0, iRefLine8.memoryValue.first())
 
-        assertTrue(iRefLine10.memoryAddress.size == 1)
-        assertTrue(iRefLine10.memoryAddress.first() == iDecl.memoryAddress)
-        assertTrue(iRefLine10.memoryValue.size == 1)
-        assertTrue(iRefLine10.memoryValue.first() == literal2)
+        // Line 9
+        assertEquals(1, iRefLine9.memoryAddress.size)
+        assertEquals(iDecl.memoryAddress, iRefLine9.memoryAddress.first())
+        assertEquals(1, iRefLine9.memoryValue.size)
+        assertEquals(literal2, iRefLine9.memoryValue.filterIsInstance<Literal<*>>().first())
 
-        assertTrue(iRefLine11.memoryAddress.size == 1)
-        assertTrue(iRefLine11.memoryAddress.first() == iDecl.memoryAddress)
-        // TODO: fix this assertTrue(iRefLine11.memoryValue.size == 1)
+        // Line 10
+        assertEquals(1, iRefLine10.memoryAddress.size)
+        assertEquals(iDecl.memoryAddress, iRefLine10.memoryAddress.first())
+        assertEquals(1, iRefLine10.memoryValue.size)
+        assertEquals(literal2, iRefLine10.memoryValue.first())
+
+        // Line 11
+        assertEquals(1, iRefLine11.memoryAddress.size)
+        assertEquals(iDecl.memoryAddress, iRefLine11.memoryAddress.first())
+        assertEquals(1, iRefLine11.memoryValue.size)
         assertTrue(iRefLine11.memoryValue.filterIsInstance<BinaryOperator>().isNotEmpty())
 
-        assertTrue(aPointerDerefLine12.memoryAddress.size == 1)
-        assertTrue(aPointerDerefLine12.memoryAddress.first() == iDecl.memoryAddress)
-        assertTrue(aPointerDerefLine12.memoryValue.size == 1)
+        // Line 12
+        assertEquals(1, aPointerDerefLine12.memoryAddress.size)
+        assertEquals(iDecl.memoryAddress, aPointerDerefLine12.memoryAddress.first())
+        assertEquals(1, aPointerDerefLine12.memoryValue.size)
         assertTrue(aPointerDerefLine12.memoryValue.filterIsInstance<BinaryOperator>().isNotEmpty())
+
+        // Line 13 should only update the DeclarationState, not much here to test
+        // Line 14
+        assertEquals(1, aPointerDerefLine14.memoryAddress.size)
+        assertEquals(iDecl.memoryAddress, aPointerDerefLine14.memoryAddress.first())
+        assertEquals(1, aPointerDerefLine14.memoryValue.size)
+        assertEquals(iUO, aPointerDerefLine14.memoryValue.first())
+
+        // Line 15
+        assertTrue(jPointerRef.memoryAddress.isEmpty())
+        assertEquals(1, jPointerRef.memoryValue.size)
+        assertEquals(jDecl.memoryAddress, jPointerRef.memoryValue.first())
+        assertEquals(1, aRefLine15.memoryAddress.size)
+        assertEquals(aDecl.memoryAddress, aRefLine15.memoryAddress.first())
+        assertEquals(1, aRefLine15.memoryValue.size)
+        assertEquals(jDecl.memoryAddress, aRefLine15.memoryValue.first())
+
+        // Line 16
+        assertEquals(1, aPointerDerefLine16.memoryAddress.size)
+        assertEquals(jDecl.memoryAddress, aPointerDerefLine16.memoryAddress.first())
+        assertEquals(1, aPointerDerefLine16.memoryValue.size)
+        assertEquals(literal1, aPointerDerefLine16.memoryValue.first())
+
+        // Line 17
+        assertEquals(1, aPointerDerefLine17.memoryAddress.size)
+        assertEquals(jDecl.memoryAddress, aPointerDerefLine17.memoryAddress.first())
+        assertEquals(1, aPointerDerefLine17.memoryValue.size)
+        assertEquals(literal3, aPointerDerefLine17.memoryValue.first())
+
+        // Line 18
+        assertEquals(1, bPointerDerefLine18.memoryAddress.size)
+        assertEquals(iDecl.memoryAddress, bPointerDerefLine18.memoryAddress.first())
+        assertEquals(1, bPointerDerefLine18.memoryValue.size)
+        assertEquals(iUO, bPointerDerefLine18.memoryValue.first())
     }
 }
---
+++ cpg-language-cxx/src/test/resources/pointsto.cpp
@@ -0,0 +1,98 @@
+#include <stdio.h>
+
+int main() {
+  int i=0;
+  int j=1;
+  int* a=&i;
+  int* b=a;
+  printf("%d\n", i);
+  i=2;
+  printf("%d\n", i);
+  i = i + 1;
+  printf("%d\n", *a);
+  i++;
+  printf("%d\n", *a);
+  a=&j;
+  printf("%d\n", *a);
+  *a=3;
+  printf("%d\n", *b);
+}
+
+/*int conditions() {
+  int i=0;                                                                                                                                            
+  int j=1;                                                                      
+  int* a;                                                                                                                                                                                                                                           
+  if ( 1 == 1 ) {
+    a = &i;
+  } else {
+    a = &j;
+  }
+  printf("%d\n", *a);
+
+  for (int x=0; x<10; x++) {
+    i++;
+  }
+  printf("%d\n", *a);
+}
+
+
+int memcpy() {
+  char a[3]="aa";
+  char b[3]="bb";
+  char c[3]="cc";
+
+  char* pa=a;
+
+  char* noalias1=b;
+  char* noalias2=b;
+  char* noalias3=b;
+  char* noalias4=b;
+
+  char* alias1=c;
+  char* alias2=c; 
+  char* alias3=c;
+  char* alias4=c; 
+  
+//  printf("%s, %s, %s, %p, %p, %p\n", pa, pb, pc, pa, pb, pc);
+
+  // No aliases
+  memcpy(noalias1,pa,sizeof(pa));
+  memcpy_verw_s(noalias2, sizeof(pa), pa, sizeof(pa));
+  memcpy_s(noalias3, sizeof(pa), &pa, sizeof(pa));
+  memcpy_verw(&noalias4, pa, sizeof(pa));
+
+  // Create aliases
+  memcpy(&alias1, &pa, sizeof(pa));
+  printf("%s\n", pa);
+  memcpy_verw_s(&alias2, sizeof(pa), &pa, sizeof(pa));
+  memcpy_verw(&alias3, &pa, sizeof(pa));
+  memcpy_s(&alias4, sizeof(pa), &pa, sizeof(pa));
+  
+  printf("%c %c %c %c\n", *alias1, *alias2, *alias3, *alias4);
+}
+
+int arrays() {
+   int n[5];   
+   int i, j;
+          
+   for(i = 0; i < 5; i++){
+      n[i] = i + 100;
+   }
+   
+   for(j = 0; j < 5; j++){
+      printf("n[%d] = %d\n", j, n[j]);
+   }
+   return 0;
+
+} 
+
+int ghidra(undefined8 *param_1) {
+  undefined8 uVar1;   
+  undefined8 *local_10;
+                                                                                                                                   
+  uVar1 = DAT_0011b1c8;                                                                                                                                  
+  *param_1 = CONCAT71(key._1_7_,(char)key);                                                                                                            
+  param_1[1] = uVar1;
+  
+  *local_10[0] = key;                                                                                                                                       local_10[1] = uVar1;
+}*/
