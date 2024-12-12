065f663939181a9b8ba251eed2b07221e4a20e77
Christian Banse <oxisto@clouditor.io>
2024-12-12 20:40:21 +0100

    typo

--- cpg-language-cxx/src/main/kotlin/de/fraunhofer/aisec/cpg/frontends/cxx/DeclarationHandler.kt
+++ cpg-language-cxx/src/main/kotlin/de/fraunhofer/aisec/cpg/frontends/cxx/DeclarationHandler.kt
@@ -245,7 +245,7 @@ class DeclarationHandler(lang: CXXLanguageFrontend) :
             return if (this.declSpecifier is IASTCompositeTypeSpecifier) {
                 if (this.declSpecifier.rawSignature.contains("typedef")) {
                     // This is very stupid. For composite type specifiers, we need to make sure that
-                    // we do not match simple because our declarations contain a typedef.
+                    // we do not match simply because our declarations contain a typedef.
                     // The problem is that we cannot correctly detect the case where both our "main"
                     // declaration and our sub declarations contain a typedef :(
                     (this.declSpecifier as IASTCompositeTypeSpecifier).getDeclarations(true).none {
