2b19f8c1ab84f4494f34a71261c26a5efd07b2f3
Maximilian Kaul <maximilian.kaul@aisec.fraunhofer.de>
2025-01-14 17:03:42 +0100

    use find for dependency

--- cpg-concepts/build.gradle.kts
+++ cpg-concepts/build.gradle.kts
@@ -43,6 +43,10 @@ dependencies {
     // to evaluate some test cases
     testImplementation(project(":cpg-analysis"))
 
-    // to evaluate some test cases
-    integrationTestImplementation(project(":cpg-language-python"))
+    // We depend on the C++ frontend for the integration tests, but the frontend is only available if enabled.
+    // If it's not available, the integration tests fail (which is ok). But if we would directly reference the
+    // project here, the build system would fail any task since it will not find a non-enabled project.
+    findProject(":cpg-language-python")?.also {
+        integrationTestImplementation(it)
+    }
 }
