625da1f6c813107bef7880cb818522e4a094b8c7
Maximilian Kaul <maximilian.kaul@aisec.fraunhofer.de>
2025-01-15 14:10:43 +0100

    make concepts available in neo4j and the console

--- cpg-neo4j/build.gradle.kts
+++ cpg-neo4j/build.gradle.kts
@@ -81,6 +81,6 @@ dependencies {
     findProject(":cpg-language-cxx")?.also {
         integrationTestImplementation(it)
     }
-
     integrationTestImplementation(project(":cpg-concepts"))
+    implementation(project(":cpg-concepts"))
 }
