7192e03dc7bfe28ab616e9e1916e02f9dd8fe544
Christian Banse <christian.banse@aisec.fraunhofer.de>
2025-01-26 12:13:55 +0100

    Adjusted SARIF locations for methods

--- codyze-core/src/main/kotlin/de/fraunhofer/aisec/codyze/Sarif.kt
+++ codyze-core/src/main/kotlin/de/fraunhofer/aisec/codyze/Sarif.kt
@@ -26,8 +26,15 @@
 package de.fraunhofer.aisec.codyze
 
 import de.fraunhofer.aisec.cpg.graph.Node
+import de.fraunhofer.aisec.cpg.graph.declarations.Declaration
+import de.fraunhofer.aisec.cpg.graph.declarations.FieldDeclaration
 import de.fraunhofer.aisec.cpg.graph.declarations.FunctionDeclaration
+import de.fraunhofer.aisec.cpg.graph.declarations.NamespaceDeclaration
+import de.fraunhofer.aisec.cpg.graph.declarations.ParameterDeclaration
+import de.fraunhofer.aisec.cpg.graph.declarations.VariableDeclaration
 import de.fraunhofer.aisec.cpg.graph.firstParentOrNull
+import de.fraunhofer.aisec.cpg.graph.statements.expressions.Literal
+import de.fraunhofer.aisec.cpg.graph.statements.expressions.Reference
 import de.fraunhofer.aisec.cpg.query.QueryTree
 import io.github.detekt.sarif4k.*
 
@@ -109,15 +116,68 @@ private fun Node.toSarifCallStack(): List<Stack> {
 private fun List<Node>?.toSarifThreadFlowLocation(): List<ThreadFlowLocation> {
     return this?.mapIndexed { idx, node ->
         ThreadFlowLocation(
-            location = node.toSarifLocation(message = node.toSarifMessage()),
+            location =
+                node.toSarifLocation(message = node.toSarifMessage(), onlyFunctionHeader = true),
             executionOrder = idx.toLong(),
         )
     } ?: listOf()
 }
 
 /** Converts a [Node.location] to a [Location]. */
-fun Node?.toSarifLocation(message: Message? = this.toSarifMessage()): Location? {
-    return this?.location?.let { Location(physicalLocation = it.toSarif(), message = message) }
+fun Node?.toSarifLocation(
+    message: Message? = this.toSarifMessage(),
+    /**
+     * If this option is set to true, we only emit the location of the function header, not the
+     * entire function body.
+     *
+     * This is helpful for cases where we want to highlight the function declaration in the code
+     * editor, but not the entire function body.
+     */
+    onlyFunctionHeader: Boolean = false,
+): Location? {
+    val location = this?.location
+    if (location == null) {
+        return null
+    }
+
+    return if (this is FunctionDeclaration && this.body != null && onlyFunctionHeader) {
+            // Try to calculate the end of the header by using the beginning of the body. This is
+            // not entirely correct since in some programming languages we need to start the body
+            // location at the first statement, since we are missing location information for the
+            // body "block", but it's the best we can do
+            val location =
+                de.fraunhofer.aisec.cpg.sarif.PhysicalLocation(
+                    uri = location.artifactLocation.uri,
+                    region =
+                        de.fraunhofer.aisec.cpg.sarif.Region(
+                            startLine = location.region.startLine,
+                            startColumn = location.region.startColumn,
+                            endLine =
+                                this.body?.location?.region?.startLine ?: location.region.endLine,
+                            endColumn =
+                                this.body?.location?.region?.startColumn
+                                    ?: location.region.endColumn,
+                        ),
+                )
+            location
+        } else {
+            this.location
+        }
+        ?.let {
+            Location(
+                physicalLocation = it.toSarif(),
+                logicalLocations =
+                    listOf(
+                        LogicalLocation(
+                            fullyQualifiedName =
+                                if (this is Declaration) this.name.toString() else null,
+                            name = this.name.localName,
+                            kind = this.toSarifKind(),
+                        )
+                    ),
+                message = message,
+            )
+        }
 }
 
 /** Converts a [de.fraunhofer.aisec.cpg.sarif.PhysicalLocation] to a [PhysicalLocation]. */
@@ -132,4 +192,22 @@ fun de.fraunhofer.aisec.cpg.sarif.PhysicalLocation.toSarif(): PhysicalLocation {
                 endColumn = this.region.endColumn.toLong(),
             ),
     )
+}
+
+/**
+ * Converts a [Node] to a well-known SARIF kind. This is used to categorize the node in the SARIF
+ * output.
+ */
+private fun Node.toSarifKind(): String? {
+    return when (this) {
+        is FunctionDeclaration -> "function"
+        is FieldDeclaration -> "member"
+        is NamespaceDeclaration -> "namespace"
+        is ParameterDeclaration -> "parameter"
+        is VariableDeclaration -> "variable"
+        is Declaration -> "declaration"
+        is Reference -> "object"
+        is Literal<*> -> "value"
+        else -> null
+    }
 }
