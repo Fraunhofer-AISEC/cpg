64bfb2db051aa9d186f0e37bcc5358acd607399c
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-02-14 16:06:30 +0100

    Test for intraprocedural backwards analyses

--- cpg-analysis/src/test/kotlin/de/fraunhofer/aisec/cpg/query/FlowQueriesTest.kt
+++ cpg-analysis/src/test/kotlin/de/fraunhofer/aisec/cpg/query/FlowQueriesTest.kt
@@ -27,35 +27,18 @@ package de.fraunhofer.aisec.cpg.query
 
 import de.fraunhofer.aisec.cpg.TranslationConfiguration
 import de.fraunhofer.aisec.cpg.frontends.TestLanguage
-import de.fraunhofer.aisec.cpg.graph.Node
-import de.fraunhofer.aisec.cpg.graph.array
-import de.fraunhofer.aisec.cpg.graph.builder.assign
-import de.fraunhofer.aisec.cpg.graph.builder.body
-import de.fraunhofer.aisec.cpg.graph.builder.call
-import de.fraunhofer.aisec.cpg.graph.builder.condition
-import de.fraunhofer.aisec.cpg.graph.builder.declare
-import de.fraunhofer.aisec.cpg.graph.builder.eq
-import de.fraunhofer.aisec.cpg.graph.builder.function
-import de.fraunhofer.aisec.cpg.graph.builder.ifStmt
-import de.fraunhofer.aisec.cpg.graph.builder.literal
-import de.fraunhofer.aisec.cpg.graph.builder.param
-import de.fraunhofer.aisec.cpg.graph.builder.plus
-import de.fraunhofer.aisec.cpg.graph.builder.ref
-import de.fraunhofer.aisec.cpg.graph.builder.returnStmt
-import de.fraunhofer.aisec.cpg.graph.builder.t
-import de.fraunhofer.aisec.cpg.graph.builder.thenStmt
-import de.fraunhofer.aisec.cpg.graph.builder.translationResult
-import de.fraunhofer.aisec.cpg.graph.builder.translationUnit
-import de.fraunhofer.aisec.cpg.graph.builder.variable
-import de.fraunhofer.aisec.cpg.graph.builder.void
+import de.fraunhofer.aisec.cpg.graph.*
+import de.fraunhofer.aisec.cpg.graph.builder.*
 import de.fraunhofer.aisec.cpg.graph.declarations.FunctionDeclaration
-import de.fraunhofer.aisec.cpg.graph.firstParentOrNull
-import de.fraunhofer.aisec.cpg.graph.literals
+import de.fraunhofer.aisec.cpg.graph.statements.expressions.BinaryOperator
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.CallExpression
+import de.fraunhofer.aisec.cpg.graph.statements.expressions.Literal
+import de.fraunhofer.aisec.cpg.graph.statements.expressions.Reference
 import de.fraunhofer.aisec.cpg.test.assertLocalName
 import de.fraunhofer.aisec.cpg.testcases.testFrontend
 import kotlin.test.Test
 import kotlin.test.assertFalse
+import kotlin.test.assertIs
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
@@ -104,11 +87,12 @@ class FlowQueriesTest {
         }
 
     @Test
-    fun testNextDFGIntraprocedurally() {
+    fun testIntraproceduralForwardDFG() {
         val result = verySimpleDataflow()
         val literal5 = result.literals.singleOrNull { it.value == 5 }
-        // Intraprocedural forward may analysis. The rest doesn't matter
         assertNotNull(literal5)
+
+        // Intraprocedural forward may analysis. The rest doesn't matter
         val queryResultMay =
             dataFlowBase(
                 startNode = literal5,
@@ -137,7 +121,6 @@ class FlowQueriesTest {
         }
 
         // Intraprocedural forward may analysis. The rest doesn't matter
-        assertNotNull(literal5)
         val queryResultMust =
             dataFlowBase(
                 startNode = literal5,
@@ -150,6 +133,84 @@ class FlowQueriesTest {
         assertFalse(
             queryResultMust.value,
             "For the MUST analysis, we cannot ignore the then statement which violates that the data arrive in baz.",
+        )
+        queryResultMust.children.forEach {
+            // There are multiple paths which have their own query tree. The children here hold the
+            // list of visited nodes in the value.
+            val path = it.children.singleOrNull()?.value as? List<Node>
+            assertNotNull(path, "There should be a path represented by a list of nodes")
+            path.forEach { node ->
+                assertLocalName(
+                    "main",
+                    node.firstParentOrNull<FunctionDeclaration>(),
+                    "We expect that all nodes are within the function \"main\". I.e., there's no node in foo.",
+                )
+            }
+        }
+    }
+
+    @Test
+    fun testIntraproceduralBackwardDFG() {
+        val result = verySimpleDataflow()
+        val bazCall = result.calls["baz"]
+        assertNotNull(bazCall, "There is exactly one call to a function called baz")
+        val addition = bazCall.arguments.singleOrNull()
+        assertIs<BinaryOperator>(
+            addition,
+            "There should be a single argument for baz which is a binary operator",
+        )
+        val bazARef = addition.lhs
+        assertIs<Reference>(
+            bazARef,
+            "The lhs of the addition is expected to be a Reference to \"a\"",
+        )
+        assertLocalName(
+            "a",
+            bazARef,
+            "The lhs of the addition is expected to be a Reference to \"a\"",
+        )
+
+        // Intraprocedural backward may analysis. The rest doesn't matter
+        val queryResultMay =
+            dataFlowBase(
+                startNode = bazARef,
+                direction = AnalysisDirection.BACKWARD,
+                scope = INTRAPROCEDURAL(),
+                type = AnalysisType.MAY,
+                predicate = { (it as? Literal<*>)?.value == 5 },
+            )
+        print(queryResultMay.printNicely())
+        assertTrue(
+            queryResultMay.value,
+            "For the MAY analysis, we can ignore the then statement which would violate that the \"5\" would arrive in baz.",
+        )
+        queryResultMay.children.forEach {
+            // There are multiple paths which have their own query tree. The children here hold the
+            // list of visited nodes in the value.
+            val path = it.children.singleOrNull()?.value as? List<Node>
+            assertNotNull(path, "There should be a path represented by a list of nodes")
+            path.forEach { node ->
+                assertLocalName(
+                    "main",
+                    node.firstParentOrNull<FunctionDeclaration>(),
+                    "We expect that all nodes are within the function \"main\". I.e., there's no node in foo.",
+                )
+            }
+        }
+
+        // Intraprocedural forward may analysis. The rest doesn't matter
+        val queryResultMust =
+            dataFlowBase(
+                startNode = bazARef,
+                direction = AnalysisDirection.BACKWARD,
+                scope = INTRAPROCEDURAL(),
+                type = AnalysisType.MUST,
+                predicate = { (it as? Literal<*>)?.value == 5 },
+            )
+        print(queryResultMust.printNicely())
+        assertFalse(
+            queryResultMust.value,
+            "For the MUST analysis, we cannot ignore the then statement which violates that the \"5\" arrives in baz.",
         )
         queryResultMust.children.forEach {
             // There are multiple paths which have their own query tree. The children here hold the
