07391429563cc2e003d725645bf0b261120e921f
Maximilian Kaul <maximilian.kaul@aisec.fraunhofer.de>
2025-02-20 14:53:04 +0100

    Fixed `astParent` of elements in `TupleDeclaration` (#1824)

--- cpg-core/src/test/kotlin/de/fraunhofer/aisec/cpg/graph/declarations/TupleDeclarationTest.kt
+++ cpg-core/src/test/kotlin/de/fraunhofer/aisec/cpg/graph/declarations/TupleDeclarationTest.kt
@@ -36,6 +36,7 @@ import de.fraunhofer.aisec.cpg.graph.types.TupleType
 import de.fraunhofer.aisec.cpg.test.*
 import kotlin.test.Test
 import kotlin.test.assertContains
+import kotlin.test.assertEquals
 import kotlin.test.assertIs
 import kotlin.test.assertNotNull
 
@@ -68,7 +69,7 @@ class TupleDeclarationTest {
                                 newCallExpression(newReference("func")),
                             )
                         scopeManager.addDeclaration(tuple)
-                        tuple.elements.forEach { scopeManager.addDeclaration(it) }
+                        tuple.elements.forEach { scopeManager.addDeclaration(it, addToAST = false) }
 
                         function("main") { body { call("print") { ref("a") } } }
                     }
@@ -91,11 +92,13 @@ class TupleDeclarationTest {
             assertNotNull(a)
             assertLocalName("MyClass", a.type)
             assertContains(a.prevDFG, call)
+            assertEquals(tuple, a.astParent)
 
             val b = tuple.elements["b"]
             assertNotNull(b)
             assertLocalName("error", b.type)
             assertContains(b.prevDFG, call)
+            assertEquals(tuple, b.astParent)
 
             val callPrint = main.calls["print"]
             assertNotNull(callPrint)
@@ -144,7 +147,9 @@ class TupleDeclarationTest {
                                         )
                                     this.declarationEdges += tuple
                                     scopeManager.addDeclaration(tuple)
-                                    tuple.elements.forEach { scopeManager.addDeclaration(it) }
+                                    tuple.elements.forEach {
+                                        scopeManager.addDeclaration(it, addToAST = false)
+                                    }
                                 }
                                 call("print") { ref("a") }
                             }
@@ -169,11 +174,13 @@ class TupleDeclarationTest {
             assertNotNull(a)
             assertLocalName("MyClass", a.type)
             assertContains(a.prevDFG, call)
+            assertEquals(tuple, a.astParent)
 
             val b = tuple.elements["b"]
             assertNotNull(b)
             assertLocalName("error", b.type)
             assertContains(b.prevDFG, call)
+            assertEquals(tuple, b.astParent)
 
             val callPrint = main.calls["print"]
             assertNotNull(callPrint)
