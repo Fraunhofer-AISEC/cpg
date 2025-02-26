599d8dac87d9e5f170198a8eb09449ceef15af6e
Christian Banse <christian.banse@aisec.fraunhofer.de>
2025-02-26 17:04:23 +0100

    Fixed bug in `translationUnitForInference`
    There was a bug in `translationUnitForInference` where we returned an incorrect translation result.

--- cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/frontends/Language.kt
+++ cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/frontends/Language.kt
@@ -37,6 +37,7 @@ import de.fraunhofer.aisec.cpg.ancestors
 import de.fraunhofer.aisec.cpg.graph.Name
 import de.fraunhofer.aisec.cpg.graph.Node
 import de.fraunhofer.aisec.cpg.graph.OverlayNode
+import de.fraunhofer.aisec.cpg.graph.component
 import de.fraunhofer.aisec.cpg.graph.declarations.Declaration
 import de.fraunhofer.aisec.cpg.graph.declarations.FunctionDeclaration
 import de.fraunhofer.aisec.cpg.graph.declarations.NamespaceDeclaration
@@ -405,8 +406,8 @@ abstract class Language<T : LanguageFrontend<*, *>> : Node() {
      *
      * Therefore, we give the language a chance to return a [TranslationUnitDeclaration] where the
      * declaration should be placed. If the language does not override this function, the default
-     * implementation will return the first [TranslationUnitDeclaration] in
-     * [TranslationContext.currentComponent].
+     * implementation will return the first [TranslationUnitDeclaration] in the component of
+     * [source].
      *
      * But languages might choose to take the information of [TypeToInfer] and [source] and create a
      * specific [TranslationUnitDeclaration], e.g., for each namespace that is inferred globally or
@@ -416,10 +417,21 @@ abstract class Language<T : LanguageFrontend<*, *>> : Node() {
      * @param source the source that was responsible for the inference
      */
     fun <TypeToInfer : Node> translationUnitForInference(source: Node): TranslationUnitDeclaration {
-        val tu = source.ctx?.currentComponent?.translationUnits?.firstOrNull()
+        // The easiest way for the current component would be the traversing the AST, but that does
+        // not work for types. But types have a scope and the scope (should) have the connection to
+        // the AST
+        val component =
+            if (source is Type) {
+                source.scope?.astNode?.component
+            } else {
+                source.component
+            }
+        // We should also make sure that the language matches
+        val tu = component?.translationUnits?.firstOrNull { it.language == this }
+
         if (tu == null) {
             throw TranslationException(
-                "No translation unit found that should be used for inference"
+                "No suitable translation unit found that should be used for inference"
             )
         }
 
