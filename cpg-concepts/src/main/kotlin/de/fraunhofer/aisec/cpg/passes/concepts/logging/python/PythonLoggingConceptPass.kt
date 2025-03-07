a589d90700be4eeb1d26fa65afa4f05c77d7adce
Maximilian Kaul <maximilian.kaul@aisec.fraunhofer.de>
2025-03-07 14:07:49 +0100

    bump

--- cpg-concepts/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/concepts/logging/python/PythonLoggingConceptPass.kt
+++ cpg-concepts/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/concepts/logging/python/PythonLoggingConceptPass.kt
@@ -26,17 +26,24 @@
 package de.fraunhofer.aisec.cpg.passes.concepts.logging.python
 
 import de.fraunhofer.aisec.cpg.TranslationContext
-import de.fraunhofer.aisec.cpg.graph.*
+import de.fraunhofer.aisec.cpg.graph.Backward
+import de.fraunhofer.aisec.cpg.graph.GraphToFollow
+import de.fraunhofer.aisec.cpg.graph.Node
+import de.fraunhofer.aisec.cpg.graph.argumentValueByNameOrPosition
 import de.fraunhofer.aisec.cpg.graph.concepts.logging.*
 import de.fraunhofer.aisec.cpg.graph.declarations.ImportDeclaration
+import de.fraunhofer.aisec.cpg.graph.declarations.TranslationUnitDeclaration
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.CallExpression
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.MemberExpression
-import de.fraunhofer.aisec.cpg.passes.ComponentPass
+import de.fraunhofer.aisec.cpg.helpers.Util
 import de.fraunhofer.aisec.cpg.passes.DFGPass
 import de.fraunhofer.aisec.cpg.passes.EvaluationOrderGraphPass
 import de.fraunhofer.aisec.cpg.passes.SymbolResolver
+import de.fraunhofer.aisec.cpg.passes.concepts.ConceptPass
 import de.fraunhofer.aisec.cpg.passes.configuration.DependsOn
 import de.fraunhofer.aisec.cpg.passes.configuration.ExecuteLate
+import de.fraunhofer.aisec.cpg.query.dataFlow
+import de.fraunhofer.aisec.cpg.query.successfulLastNodes
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -48,13 +55,11 @@ import org.slf4j.LoggerFactory
 @DependsOn(SymbolResolver::class)
 @DependsOn(EvaluationOrderGraphPass::class)
 @DependsOn(DFGPass::class)
-class PythonLoggingConceptPass(ctx: TranslationContext) : ComponentPass(ctx) {
-    private var log: Logger = LoggerFactory.getLogger(this.javaClass)
+class PythonLoggingConceptPass(ctx: TranslationContext) : ConceptPass(ctx) {
 
     /**
-     * A storage connecting CPG nodes with [de.fraunhofer.aisec.cpg.graph.concepts.logging.Log]s.
-     * This is used to connect logging calls to the matching
-     * [de.fraunhofer.aisec.cpg.graph.concepts.logging.Log].
+     * A storage connecting CPG nodes with [Log]s. This is used to connect logging calls to the
+     * matching [Log].
      *
      * The key corresponds to the loggers name. The following all map to an empty string (matching
      * Pythons behavior):
@@ -68,25 +73,17 @@ class PythonLoggingConceptPass(ctx: TranslationContext) : ComponentPass(ctx) {
      */
     private val loggers = mutableMapOf<String, Log>()
 
-    private val DEFAULT_LOGGER_NAME = ""
+    private val DEFAULT_LOGGER_NAME = "logging"
 
-    /** The global `import logging` node. */
-    private var loggingLogger: ImportDeclaration? = null
-
-    override fun cleanup() {
-        // nothing to do
-    }
-
-    /**
-     * This pass is interested in [ImportDeclaration]s and
-     * [de.fraunhofer.aisec.cpg.graph.statements.expressions.CallExpression]s as these are the
-     * relevant parts of the Python code for logging.
-     */
-    override fun accept(comp: Component) {
-        loggingLogger =
-            comp.imports.singleOrNull { import -> import.import.toString() == "logging" }
-        comp.imports.forEach { import -> handleImport(import) }
-        comp.calls.forEach { call -> handleCall(call) }
+    override fun handleNode(node: Node, tu: TranslationUnitDeclaration) {
+        when (node) {
+            is ImportDeclaration -> {
+                handleImport(node)
+            }
+            is CallExpression -> {
+                handleCall(node)
+            }
+        }
     }
 
     /**
@@ -121,73 +118,80 @@ class PythonLoggingConceptPass(ctx: TranslationContext) : ComponentPass(ctx) {
      * - `logging.critical(...)` (and similar for `error` / `warn` / ...) is translated to a
      *   [de.fraunhofer.aisec.cpg.graph.concepts.logging.LogWrite]
      *
-     * @param callExpression The
-     *   [de.fraunhofer.aisec.cpg.graph.statements.expressions.CallExpression] to handle
+     * @param call The [CallExpression] to handle
      * @return n/a (The new node is created and added to the graph)
      */
-    private fun handleCall(callExpression: CallExpression) {
-        val callee = callExpression.callee
+    private fun handleCall(call: CallExpression) {
+        val callee = call.callee
 
         if (callee.name.toString() == "logging.getLogger") {
-            val loggerName = callExpression.arguments.firstOrNull()?.evaluate().toString()
+            val loggerName = call.argumentValueByNameOrPosition<String>(name = "name", position = 0)
             val normalizedLoggerName =
                 when (loggerName) {
                     "",
                     "null" // Pythons `None` is translated to a Kotlin `null`
                     -> DEFAULT_LOGGER_NAME
+                    null -> {
+                        Util.errorWithFileLocation(
+                            call,
+                            log,
+                            "Failed to parse the loggers name. Using \"{}\" as a fallback.",
+                            DEFAULT_LOGGER_NAME,
+                        )
+                        DEFAULT_LOGGER_NAME
+                    }
                     else -> loggerName
                 }
             val logger = loggers[normalizedLoggerName]
             if (logger == null) { // only add it once
-                val newNode = newLog(underlyingNode = callExpression, name = normalizedLoggerName)
+                val newNode = newLog(underlyingNode = call, name = normalizedLoggerName)
                 loggers += normalizedLoggerName to newNode
-                newLogGet(underlyingNode = callExpression, logger = newNode)
+                newLogGet(underlyingNode = call, logger = newNode)
             } else {
                 // the logger is already present -> only add a [LogGet] node
-                newLogGet(underlyingNode = callExpression, logger = logger)
+                newLogGet(underlyingNode = call, logger = logger)
             }
         } else if (callee.name.toString().startsWith("logging.")) {
-            loggers[DEFAULT_LOGGER_NAME]?.let { logOpHelper(callExpression, it) }
+            loggers[DEFAULT_LOGGER_NAME]?.let { logOpHelper(call, it) }
         } else {
             // might be a call like `logger.error`
-            val logger = findLogger(callExpression)
-            logger?.let { logOpHelper(callExpression, it) }
+            val logger = findLogger(call)
+            logger?.let { logOpHelper(call, it) }
         }
     }
 
     /**
-     * Finds the corresponding logger for the given [callExpression].
+     * Finds the corresponding logger for the given [call].
      *
-     * This function works by walking the DFG backwards on the [callExpression]s base to find a
-     * [Log].
+     * This function works by walking the DFG backwards on the [call]s base to find a [LogGet] and
+     * then via [LogGet.concept] to the actual [Log] node.
      *
-     * @param callExpression The call to handle.
+     * @param call The call to handle.
      * @return The [Log] if found.
      */
-    private fun findLogger(callExpression: CallExpression): Log? {
-        val callee = callExpression.callee
+    private fun findLogger(call: CallExpression): Log? {
+        val callee = call.callee
         if (callee is MemberExpression) {
             // might be a call like `logger.error`
             val base = callee.base
-            val fulfilledPaths: List<List<Node>> =
-                base
-                    .followPrevFullDFGEdgesUntilHit(collectFailedPaths = false) {
-                        it.overlays.any { overlay ->
-                            overlay is LogGet
-                        } // we are logging for a node which has a [LogGet] attached to it
-                    }
-                    .fulfilled
+            val pathsToLogGet =
+                dataFlow(startNode = base, direction = Backward(GraphToFollow.DFG)) {
+                    it.overlays.any { overlay ->
+                        overlay is LogGet
+                    } // we are logging for a node which has a [LogGet] attached to it
+                }
+
             val loggers =
-                fulfilledPaths
-                    .map { path ->
-                        path.last()
-                    } // we're interested in the last node of the path, i.e. the node connected to
-                    // the [LogGet] node
-                    .flatMap { it.overlays } // move to the "overlays" world
+                pathsToLogGet
+                    .successfulLastNodes()
+                    .flatMap {
+                        it.overlays // move to the "overlays" world
+                    }
                     .filterIsInstance<LogGet>() // discard not-relevant overlays
                     .map {
-                        it.concept
-                    } // move from [LogGet] to the corresponding [Log] concept node
+                        it.concept // move from [LogGet] to the corresponding [Log] concept node
+                    }
+
             if (loggers.size > 1) {
                 log.error("Found multiple loggers. Selecting one at random.")
             }
@@ -202,12 +206,12 @@ class PythonLoggingConceptPass(ctx: TranslationContext) : ComponentPass(ctx) {
      *
      * A warning is logged if the call cannot be handled (i.e. not implemented).
      *
-     * @param callExpression The underlying [CallExpression] to handle.
+     * @param call The underlying [CallExpression] to handle.
      * @param logger The [Log] this call expression operates on.
      * @return n/a (The new node is created and added to the graph)
      */
-    private fun logOpHelper(callExpression: CallExpression, logger: Log) {
-        val callee = callExpression.callee
+    private fun logOpHelper(call: CallExpression, logger: Log) {
+        val callee = call.callee
         when (callee.name.localName.toString()) {
             "fatal",
             "critical",
@@ -216,26 +220,24 @@ class PythonLoggingConceptPass(ctx: TranslationContext) : ComponentPass(ctx) {
             "warning",
             "info",
             "debug" -> {
-                val name = callExpression.name.localName.toString()
+                val name = call.name.localName.toString()
                 val lvl = logLevelStringToEnum(name)
                 newLogWrite(
-                    underlyingNode = callExpression,
+                    underlyingNode = call,
                     logger = logger,
-                    logArguments = callExpression.arguments,
+                    logArguments = call.arguments,
                     level = lvl,
                 )
             }
             else -> {
-                log.warn("Found an unhandled logging call: \"$callExpression\".")
+                log.warn("Found an unhandled logging call: \"$call\".")
             }
         }
     }
 
     /**
-     * Maps a string "critical" / ... to the corresponding
-     * [de.fraunhofer.aisec.cpg.graph.concepts.logging.LogLevel].
-     * [de.fraunhofer.aisec.cpg.graph.concepts.logging.LogLevel.UNKNOWN] is used when the
-     * translation fails and a warning is logged.
+     * Maps a string "critical" / ... to the corresponding [LogLevel]. [LogLevel.UNKNOWN] is used
+     * when the translation fails and a warning is logged.
      *
      * @param loglevel The string to parse.
      * @return The corresponding log level.
@@ -255,5 +257,9 @@ class PythonLoggingConceptPass(ctx: TranslationContext) : ComponentPass(ctx) {
                 LogLevel.UNKNOWN
             }
         }
+    }
+
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(this.javaClass)
     }
 }
