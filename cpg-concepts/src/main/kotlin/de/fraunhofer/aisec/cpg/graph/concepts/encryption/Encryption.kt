b6e9fbeadc87e4aaa620d8468e8669d69b56986d
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-03-11 12:11:14 +0100

    Draft for hacky encryption pass

---
+++ cpg-concepts/src/main/kotlin/de/fraunhofer/aisec/cpg/graph/concepts/encryption/Encryption.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2025, Fraunhofer AISEC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *                    $$$$$$\  $$$$$$$\   $$$$$$\
+ *                   $$  __$$\ $$  __$$\ $$  __$$\
+ *                   $$ /  \__|$$ |  $$ |$$ /  \__|
+ *                   $$ |      $$$$$$$  |$$ |$$$$\
+ *                   $$ |      $$  ____/ $$ |\_$$ |
+ *                   $$ |  $$\ $$ |      $$ |  $$ |
+ *                   \$$$$$   |$$ |      \$$$$$   |
+ *                    \______/ \__|       \______/
+ *
+ */
+package de.fraunhofer.aisec.cpg.graph.concepts.encryption
+
+import de.fraunhofer.aisec.cpg.graph.Node
+import de.fraunhofer.aisec.cpg.graph.concepts.Concept
+import de.fraunhofer.aisec.cpg.graph.concepts.Operation
+import de.fraunhofer.aisec.cpg.graph.concepts.diskEncryption.Cipher
+import de.fraunhofer.aisec.cpg.graph.concepts.diskEncryption.Secret
+
+/** This concept represents an encrypted disk. */
+class Encryption(underlyingNode: Node) : Concept(underlyingNode = underlyingNode), IsEncryption {
+
+    /** The cipher suite used for disk encryption */
+    var cipher: Cipher? = null
+
+    /** The encryption key used for disk encryption */
+    var key: Secret? = null
+}
+
+class EncryptData(underlyingNode: Node, concept: Encryption) :
+    Operation(underlyingNode = underlyingNode, concept = concept), IsEncryption {
+    var dataToEncrypt: Node? = null
+    var encryptedData: Node? = null
+}
