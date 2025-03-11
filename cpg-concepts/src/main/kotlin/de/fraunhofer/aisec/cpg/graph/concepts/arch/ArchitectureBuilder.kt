25dd8185056621cf76b55e0501d0c50561e94154
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-03-11 18:32:47 +0100

    Architecture builder

---
+++ cpg-concepts/src/main/kotlin/de/fraunhofer/aisec/cpg/graph/concepts/arch/ArchitectureBuilder.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2025, Fraunhofer AISEC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *                    $$$$$$\  $$$$$$$\   $$$$$$\
+ *                   $$  __$$\ $$  __$$\ $$  __$$\
+ *                   $$ /  \__|$$ |  $$ |$$ /  \__|
+ *                   $$ |      $$$$$$$  |$$ |$$$$\
+ *                   $$ |      $$  ____/ $$ |\_$$ |
+ *                   $$ |  $$\ $$ |      $$ |  $$ |
+ *                   \$$$$$   |$$ |      \$$$$$   |
+ *                    \______/ \__|       \______/
+ *
+ */
+package de.fraunhofer.aisec.cpg.graph.concepts.arch
+
+import de.fraunhofer.aisec.cpg.graph.MetadataProvider
+import de.fraunhofer.aisec.cpg.graph.Node
+import de.fraunhofer.aisec.cpg.graph.concepts.newConcept
+
+fun MetadataProvider.newAgnosticOS(underlyingNode: Node) =
+    newConcept({ Agnostic(it) }, underlyingNode = underlyingNode)
+
+fun MetadataProvider.newWin32OS(underlyingNode: Node) =
+    newConcept({ Win32(it) }, underlyingNode = underlyingNode)
+
+fun MetadataProvider.newPosixOS(underlyingNode: Node) =
+    newConcept({ POSIX(it) }, underlyingNode = underlyingNode)
+
+fun MetadataProvider.newDarwinOS(underlyingNode: Node) =
+    newConcept({ Darwin(it) }, underlyingNode = underlyingNode)
