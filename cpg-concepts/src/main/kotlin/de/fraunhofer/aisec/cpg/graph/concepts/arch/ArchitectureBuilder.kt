81448f2d6ae2af72472679f844a6107e1358b300
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-03-11 20:15:35 +0100

    OS documentation

--- cpg-concepts/src/main/kotlin/de/fraunhofer/aisec/cpg/graph/concepts/arch/ArchitectureBuilder.kt
+++ cpg-concepts/src/main/kotlin/de/fraunhofer/aisec/cpg/graph/concepts/arch/ArchitectureBuilder.kt
@@ -29,14 +29,38 @@ import de.fraunhofer.aisec.cpg.graph.MetadataProvider
 import de.fraunhofer.aisec.cpg.graph.Node
 import de.fraunhofer.aisec.cpg.graph.concepts.newConcept
 
+/**
+ * Creates a new [Agnostic] concept.
+ *
+ * @param underlyingNode The underlying node representing this concept.
+ * @return The created [Agnostic] concept.
+ */
 fun MetadataProvider.newAgnosticOS(underlyingNode: Node) =
     newConcept(::Agnostic, underlyingNode = underlyingNode)
 
+/**
+ * Creates a new [Win32] concept.
+ *
+ * @param underlyingNode The underlying node representing this concept.
+ * @return The created [Win32] concept.
+ */
 fun MetadataProvider.newWin32OS(underlyingNode: Node) =
     newConcept(::Win32, underlyingNode = underlyingNode)
 
+/**
+ * Creates a new [POSIX] concept.
+ *
+ * @param underlyingNode The underlying node representing this concept.
+ * @return The created [POSIX] concept.
+ */
 fun MetadataProvider.newPosixOS(underlyingNode: Node) =
     newConcept(::POSIX, underlyingNode = underlyingNode)
 
+/**
+ * Creates a new [Darwin] concept.
+ *
+ * @param underlyingNode The underlying node representing this concept.
+ * @return The created [Darwin] concept.
+ */
 fun MetadataProvider.newDarwinOS(underlyingNode: Node) =
     newConcept(::Darwin, underlyingNode = underlyingNode)
