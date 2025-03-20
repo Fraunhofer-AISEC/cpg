b3a82588e73fcc4203f587d5d75c0f247929cc18
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-03-20 13:42:33 +0100

    Migrate to new lattice systemg

--- cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/SymbolResolverEOGIteration.kt
+++ cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/SymbolResolverEOGIteration.kt
@@ -31,7 +31,7 @@ import de.fraunhofer.aisec.cpg.frontends.Language
 import de.fraunhofer.aisec.cpg.graph.Node
 import de.fraunhofer.aisec.cpg.graph.declarations.Declaration
 import de.fraunhofer.aisec.cpg.graph.declarations.FunctionDeclaration
-import de.fraunhofer.aisec.cpg.graph.edges.Edge
+import de.fraunhofer.aisec.cpg.graph.edges.flows.EvaluationOrder
 import de.fraunhofer.aisec.cpg.graph.firstScopeParentOrNull
 import de.fraunhofer.aisec.cpg.graph.scopes.NameScope
 import de.fraunhofer.aisec.cpg.graph.scopes.RecordScope
@@ -42,34 +42,57 @@ import de.fraunhofer.aisec.cpg.graph.statements.expressions.MemberExpression
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.Reference
 import de.fraunhofer.aisec.cpg.helpers.IdentitySet
 import de.fraunhofer.aisec.cpg.helpers.LatticeElement
-import de.fraunhofer.aisec.cpg.helpers.State
 import de.fraunhofer.aisec.cpg.helpers.Util.infoWithFileLocation
-import de.fraunhofer.aisec.cpg.helpers.Worklist
+import de.fraunhofer.aisec.cpg.helpers.functional.Lattice
+import de.fraunhofer.aisec.cpg.helpers.functional.MapLattice
+import de.fraunhofer.aisec.cpg.helpers.functional.PowersetLattice
+import de.fraunhofer.aisec.cpg.helpers.functional.TupleLattice
 import de.fraunhofer.aisec.cpg.helpers.identitySetOf
-import de.fraunhofer.aisec.cpg.helpers.iterateEOG
-import de.fraunhofer.aisec.cpg.helpers.toIdentitySet
-import de.fraunhofer.aisec.cpg.helpers.union
 import de.fraunhofer.aisec.cpg.passes.configuration.DependsOn
 
 /**
  * Implements the [LatticeElement] for a lattice over a set of nodes. The lattice itself is
  * constructed by the powerset.
  */
-class PowersetLatticeDeclaration(override val elements: IdentitySet<Declaration>) :
-    LatticeElement<Set<Declaration>>(elements) {
-    override fun lub(other: LatticeElement<Set<Declaration>>) =
-        PowersetLatticeDeclaration(this.elements.union(other.elements))
+typealias PowersetLatticeDeclarationLattice = PowersetLattice<Declaration>
 
-    override fun duplicate(): LatticeElement<Set<Declaration>> =
-        PowersetLatticeDeclaration(this.elements.toIdentitySet())
+typealias PowersetLatticeDeclarationElement = PowersetLattice.Element<Declaration>
 
-    override fun compareTo(other: LatticeElement<Set<Declaration>>): Int {
-        return if (this.elements.containsAll(other.elements)) {
-            if (this.elements.size > (other.elements.size)) 1 else 0
-        } else {
-            -1
-        }
-    }
+typealias ScopeToDeclarationLattice = MapLattice<Scope, PowersetLatticeDeclarationElement>
+
+typealias ScopeToDeclarationElement = MapLattice.Element<Scope, PowersetLatticeDeclarationElement>
+
+typealias NodeToDeclarationLattice = MapLattice<Node, PowersetLatticeDeclarationElement>
+
+typealias NodeToDeclarationElement = MapLattice.Element<Node, PowersetLatticeDeclarationElement>
+
+typealias DeclarationStateElement =
+    TupleLattice.Element<ScopeToDeclarationElement, NodeToDeclarationElement>
+
+typealias DeclarationState = TupleLattice<ScopeToDeclarationElement, NodeToDeclarationElement>
+
+val DeclarationStateElement.symbols
+    get() = this.first
+
+val DeclarationStateElement.candidates
+    get() = this.second
+
+fun DeclarationStateElement.pushScope(
+    scope: Scope,
+    vararg elements: Declaration,
+): DeclarationStateElement {
+    val newSymbols = this.symbols.duplicate()
+    newSymbols.computeIfAbsent(scope) { PowersetLatticeDeclarationElement() }.addAll(elements)
+    return DeclarationStateElement(newSymbols, this.candidates.duplicate())
+}
+
+fun DeclarationStateElement.pushCandidate(
+    scope: Node,
+    vararg elements: Declaration,
+): DeclarationStateElement {
+    val newCandidates = this.candidates.duplicate()
+    newCandidates.computeIfAbsent(scope) { PowersetLatticeDeclarationElement() }.addAll(elements)
+    return DeclarationStateElement(this.symbols.duplicate(), newCandidates)
 }
 
 @DependsOn(EvaluationOrderGraphPass::class)
@@ -79,79 +102,67 @@ class SymbolResolverEOGIteration(ctx: TranslationContext) : EOGStarterPass(ctx) 
         // Nothing to clean up
     }
 
-    class DeclarationState : State<Node, Set<Declaration>>() {
-        val symbols: State<Scope?, Set<Declaration>> = State()
-        val candidates: State<Node, Set<Declaration>> = State()
-
-        override fun duplicate(): DeclarationState {
-            val state = DeclarationState()
-            state.symbols += this.symbols
-            state.candidates += this.candidates
-            return state
-        }
-    }
-
     override fun accept(t: Node) {
         if (t !is FunctionDeclaration) {
             return
         }
 
-        val startState = DeclarationState()
+        val lattice =
+            DeclarationState(
+                ScopeToDeclarationLattice(PowersetLatticeDeclarationLattice()),
+                NodeToDeclarationLattice(PowersetLatticeDeclarationLattice()),
+            )
+
+        val startState =
+            DeclarationStateElement(ScopeToDeclarationElement(), NodeToDeclarationElement())
 
         // Push all global symbols (sort of legacy, in the future they should also go through the
         // EOG)
-        startState.symbols.push(
+        startState.pushScope(
             ctx.scopeManager.globalScope,
-            PowersetLatticeDeclaration(
-                ctx.scopeManager.globalScope.symbols.values.flatten().toIdentitySet()
-            ),
+            *ctx.scopeManager.globalScope.symbols.values.flatten().toTypedArray(),
         )
 
         // Push all record symbols (legacy, need to visit EOG in the future)
         ctx.scopeManager
             .filterScopes { it is NameScope }
-            .forEach {
-                startState.symbols.push(
-                    it,
-                    PowersetLatticeDeclaration(it.symbols.values.flatten().toIdentitySet()),
-                )
-            }
+            .forEach { startState.pushScope(it, *it.symbols.values.flatten().toTypedArray()) }
 
-        startState.symbols.push(t.scope, PowersetLatticeDeclaration(identitySetOf()))
-        val finalState =
-            iterateEOG(t.nextEOGEdges, startState, ::transfer) as? DeclarationState ?: return
+        t.scope?.let { startState.pushScope(it) }
+        val finalState = lattice.iterateEOG(t.nextEOGEdges, startState, ::transfer)
+
         finalState.symbols.forEach { println(it) }
     }
 
     protected open fun transfer(
-        currentEdge: Edge<Node>,
-        state: State<Node, Set<Declaration>>,
-        worklist: Worklist<Edge<Node>, Node, Set<Declaration>>,
-    ): DeclarationState {
-        if (state !is DeclarationState) {
-            throw IllegalArgumentException("State must be of type DeclarationState")
-        }
+        lattice: Lattice<DeclarationStateElement>,
+        currentEdge: EvaluationOrder,
+        state: DeclarationStateElement,
+    ): DeclarationStateElement {
 
         val node = currentEdge.end
-        when (node) {
+        return when (node) {
             is Declaration -> {
                 handleDeclaration(state, node)
             }
             is Reference -> {
                 handleReference(node, state)
             }
+            else -> state
         }
-
-        return state
     }
 
-    private fun handleReference(node: Reference, state: DeclarationState) {
+    private fun handleReference(
+        node: Reference,
+        state: DeclarationStateElement,
+    ): DeclarationStateElement {
         infoWithFileLocation(
             node,
             log,
             "Resolving reference. {} scopes are active",
             state.symbols.size,
         )
+        var state = state
         // Lookup symbol here or after the final state?
         var candidates =
             if (node is MemberExpression) {
@@ -178,7 +189,7 @@ class SymbolResolverEOGIteration(ctx: TranslationContext) : EOGStarterPass(ctx) 
 
         // Let's set it here for now, but also to the final state, maybe it's helpful for
         // later
-        state.push(node, PowersetLatticeDeclaration(candidates))
+        state = state.pushCandidate(node, *candidates.toTypedArray())
         node.candidates = candidates
 
         // Now it's getting interesting! We need to make the final decision based on whether
@@ -191,17 +202,22 @@ class SymbolResolverEOGIteration(ctx: TranslationContext) : EOGStarterPass(ctx) 
             // Reference to a variable
             node.refersTo = candidates.firstOrNull()
         }
+
+        return state
     }
 
-    private fun handleDeclaration(state: DeclarationState, node: Declaration) {
+    private fun handleDeclaration(
+        state: DeclarationStateElement,
+        node: Declaration,
+    ): DeclarationStateElement {
         // Push declaration into scope
-        state.symbols.push(node.scope, PowersetLatticeDeclaration(identitySetOf(node)))
+        return node.scope?.let { state.pushScope(it, node) } ?: state
     }
 }
 
 // TODO: we could do this easier if we would have a lattice that combines the symbols across the
 //  scopes and doing the shadowing
-private fun State<Scope?, Set<Declaration>>.resolveSymbol(
+private fun ScopeToDeclarationElement.resolveSymbol(
     symbol: Symbol,
     language: Language<*>,
     startScope: Scope,
@@ -214,7 +230,7 @@ private fun State<Scope?, Set<Declaration>>.resolveSymbol(
     val modifiedScoped = null
 
     while (scope != null) {
-        var scopeSymbols = this[scope]?.elements
+        var scopeSymbols = this[scope]
         scopeSymbols?.filterTo(symbols) { it.name.localName == symbol }
         if (symbols.isNotEmpty() == true) {
             return symbols
