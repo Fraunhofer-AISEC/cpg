2ada3fc23c1f9b5cbd8416c9416e6cbdf9b6164a
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-05-02 11:40:40 +0200

    directly calculate the finalState

--- cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/helpers/functional/BasicLatticesRedesign.kt
+++ cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/helpers/functional/BasicLatticesRedesign.kt
@@ -139,7 +139,7 @@ interface Lattice<T : Lattice.Element> {
         transformation: (Lattice<T>, EvaluationOrder, T) -> T,
     ): T {
         val globalState = IdentityHashMap<EvaluationOrder, T>()
-        val finalState = IdentityHashMap<EvaluationOrder, T>()
+        val finalState: T = this.bottom
         for (startEdge in startEdges) {
             globalState[startEdge] = startState
         }
@@ -197,13 +197,11 @@ interface Lattice<T : Lattice.Element> {
             }
 
             if (nextEdge.end.nextEOGEdges.isEmpty() || edgesList.isEmpty()) {
-                finalState[nextEdge] = newState
+                this.lub(finalState, newState, true)
             }
         }
 
-        return finalState.values.fold(finalState.values.firstOrNull()) { state, value ->
-            state?.let { lub(it, value, true) }
-        } ?: startState
+        return finalState
     }
 }
 
