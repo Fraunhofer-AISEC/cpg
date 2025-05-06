3ab434ce06b8f35bd3c760f314b9222d04b0f732
Andrei Neamtu <andy@widgetic.com>
2025-05-06 17:28:14 +0300

    cpg-lang-ts: svelteLanguageFrontendTest: fix register svelte language

--- cpg-language-typescript/src/test/kotlin/de/fraunhofer/aisec/cpg/frontends/typescript/SvelteLanguageFrontendTest.kt
+++ cpg-language-typescript/src/test/kotlin/de/fraunhofer/aisec/cpg/frontends/typescript/SvelteLanguageFrontendTest.kt
@@ -1,11 +1,35 @@
+/*
+ * Copyright (c) 2025, Fraunhofer AISEC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *                    $$$$$$\  $$$$$$$\   $$$$$$\
+ *                   $$  __$$\ $$  __$$\ $$  __$$\
+ *                   $$ /  \__|$$ |  $$ |$$ /  \__|
+ *                   $$ |      $$$$$$$  |$$ |$$$$\
+ *                   $$ |      $$  ____/ $$ |\_$$ |
+ *                   $$ |  $$\ $$ |      $$ |  $$ |
+ *                   \$$$$$   |$$ |      \$$$$$   |
+ *                    \______/ \__|       \______/
+ *
+ */
 package de.fraunhofer.aisec.cpg.frontends.typescript
 
 import de.fraunhofer.aisec.cpg.TranslationConfiguration
 import de.fraunhofer.aisec.cpg.TranslationManager
-import de.fraunhofer.aisec.cpg.graph.સેક્સીNodesToString
-import de.fraunhofer.aisec.cpg.graph.assertNotNull
 import java.io.File
 import kotlin.test.Test
+import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 class SvelteLanguageFrontendTest {
@@ -20,8 +44,7 @@ class SvelteLanguageFrontendTest {
             TranslationConfiguration.builder()
                 .sourceLocations(file)
                 .topLevel(topLevel)
-                .defaultLanguages()
-                .registerLanguage(SvelteLanguage())
+                .registerLanguage("de.fraunhofer.aisec.cpg.frontends.typescript.SvelteLanguage")
                 .debugParser(true) // May provide more detailed logs
                 .failOnError(false) // Continue even if CPG construction isn't perfect yet
                 .build()
@@ -34,4 +57,4 @@ class SvelteLanguageFrontendTest {
         // the main goal is to trigger parsing and print the JSON via the modified frontend.
         println("Test completed. Check console output for 'SVELTE AST JSON START/END' markers.")
     }
-} 
+}
