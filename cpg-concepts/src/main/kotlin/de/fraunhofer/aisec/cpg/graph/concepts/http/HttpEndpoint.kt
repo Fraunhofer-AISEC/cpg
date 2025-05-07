53db2409ea22f7df25be390ef8e835890c448175
Leutrim Shala <Leutrim.shala@aisec.fraunhofer.de>
2025-05-07 12:02:21 +0200

    Add requestContext concept

--- cpg-concepts/src/main/kotlin/de/fraunhofer/aisec/cpg/graph/concepts/http/HttpEndpoint.kt
+++ cpg-concepts/src/main/kotlin/de/fraunhofer/aisec/cpg/graph/concepts/http/HttpEndpoint.kt
@@ -30,18 +30,20 @@ import de.fraunhofer.aisec.cpg.graph.concepts.Concept
 import de.fraunhofer.aisec.cpg.graph.concepts.Operation
 import de.fraunhofer.aisec.cpg.graph.concepts.auth.Authentication
 import de.fraunhofer.aisec.cpg.graph.concepts.auth.Authorization
+import de.fraunhofer.aisec.cpg.graph.concepts.auth.RequestContext
 import de.fraunhofer.aisec.cpg.graph.concepts.flows.RemoteEntryPoint
 import de.fraunhofer.aisec.cpg.graph.declarations.FunctionDeclaration
 import java.util.Objects
 
 /** Represents a single [HttpEndpoint] on the server */
-open class HttpEndpoint(
+class HttpEndpoint(
     underlyingNode: FunctionDeclaration? = null,
     val httpMethod: HttpMethod,
     val path: String,
     val arguments: List<Node>,
     var authentication: Authentication?,
     var authorization: Authorization?,
+    var requestContext: RequestContext?,
 ) : RemoteEntryPoint(underlyingNode = underlyingNode) {
     override fun equals(other: Any?): Boolean {
         return other is HttpEndpoint &&
@@ -50,11 +52,20 @@ open class HttpEndpoint(
             other.path == this.path &&
             other.arguments == this.arguments &&
             other.authentication == this.authentication &&
-            other.authorization == this.authorization
+            other.authorization == this.authorization &&
+            other.requestContext == this.requestContext
     }
 
     override fun hashCode() =
-        Objects.hash(super.hashCode(), httpMethod, path, arguments, authentication, authorization)
+        Objects.hash(
+            super.hashCode(),
+            httpMethod,
+            path,
+            arguments,
+            authentication,
+            authorization,
+            requestContext,
+        )
 }
 
 enum class HttpMethod {
