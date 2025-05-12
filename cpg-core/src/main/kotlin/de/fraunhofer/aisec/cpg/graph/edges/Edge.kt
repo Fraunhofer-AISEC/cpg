f16529447a4761753198cede9dd79475333056a1
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2025-05-12 08:25:20 +0200

    Make assumptions a set and add collectAssumptions to  collect assumptions for final results, or propagate assumptions when declaring a dependence to an object that would not be present in the final result

--- cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/graph/edges/Edge.kt
+++ cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/graph/edges/Edge.kt
@@ -63,7 +63,7 @@ abstract class Edge<NodeType : Node> : Persistable, Cloneable, HasAssumptions {
     // Node where the edge is ingoing
     @JsonBackReference @field:EndNode var end: NodeType
 
-    @DoNotPersist override val assumptions: MutableList<Assumption> = mutableListOf()
+    @DoNotPersist override val assumptions: MutableSet<Assumption> = mutableSetOf()
 
     constructor(start: Node, end: NodeType) {
         this.start = start
