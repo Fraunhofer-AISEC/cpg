ee0846ac2f21f8f7ff2ab50dce687379a1e566de
Leutrim Shala <Leutrim.shala@aisec.fraunhofer.de>
2025-05-13 16:06:13 +0200

    Initial draft of policy test

---
+++ cpg-concepts/src/integrationTest/kotlin/de/fraunhofer/aisec/cpg/concepts/PolicyTest.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2025, Fraunhofer AISEC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *                    $$$$$$\  $$$$$$$\   $$$$$$\
+ *                   $$  __$$\ $$  __$$\ $$  __$$\
+ *                   $$ /  \__|$$ |  $$ |$$ /  \__|
+ *                   $$ |      $$$$$$$  |$$ |$$$$\
+ *                   $$ |      $$  ____/ $$ |\_$$ |
+ *                   $$ |  $$\ $$ |      $$ |  $$ |
+ *                   \$$$$$   |$$ |      \$$$$$   |
+ *                    \______/ \__|       \______/
+ *
+ */
+package de.fraunhofer.aisec.cpg.concepts
+
+import de.fraunhofer.aisec.cpg.frontends.python.PythonLanguage
+import de.fraunhofer.aisec.cpg.graph.concepts.diskEncryption.Secret
+import de.fraunhofer.aisec.cpg.graph.declarations.RecordDeclaration
+import de.fraunhofer.aisec.cpg.passes.concepts.TagOverlaysPass
+import de.fraunhofer.aisec.cpg.passes.concepts.each
+import de.fraunhofer.aisec.cpg.passes.concepts.tag
+import de.fraunhofer.aisec.cpg.passes.concepts.with
+import de.fraunhofer.aisec.cpg.test.analyze
+import kotlin.io.path.Path
+import kotlin.io.resolve
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class PolicyTest {
+    @Test
+    fun testPolicy() {
+        val topLevel = Path("src/integrationTest/resources/python")
+        val result =
+            analyze(listOf(topLevel.resolve("policy.py").toFile()), topLevel, true) {
+                it.registerLanguage<PythonLanguage>()
+                it.registerPass<TagOverlaysPass>()
+                it.configurePass<TagOverlaysPass>(
+                    TagOverlaysPass.Configuration(
+                        tag = tag { each<RecordDeclaration>().with { Secret() } }
+                    )
+                )
+            }
+        assertNotNull(result)
+    }
+}
