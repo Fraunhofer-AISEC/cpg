589915d444dff53b4ef644e4fc4e6107517c692a
Christian Banse <christian.banse@aisec.fraunhofer.de>
2025-05-23 09:11:40 +0200

    Spotless

--- codyze-core/src/integrationTest/resources/example/manual-assessments.codyze.kts
+++ codyze-core/src/integrationTest/resources/example/manual-assessments.codyze.kts
@@ -1,3 +1,29 @@
+/*
+ * Copyright (c) 2025, Fraunhofer AISEC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *                    $$$$$$\  $$$$$$$\   $$$$$$\
+ *                   $$  __$$\ $$  __$$\ $$  __$$\
+ *                   $$ /  \__|$$ |  $$ |$$ /  \__|
+ *                   $$ |      $$$$$$$  |$$ |$$$$\
+ *                   $$ |      $$  ____/ $$ |\_$$ |
+ *                   $$ |  $$\ $$ |      $$ |  $$ |
+ *                   \$$$$$   |$$ |      \$$$$$   |
+ *                    \______/ \__|       \______/
+ *
+ */
+package aa
 manualAssessment {
     of("ID-5") {
         /** Yes, I really checked this that 6 is greater than 5 */
@@ -7,9 +33,7 @@ manualAssessment {
     }
 
     of("ID-2") {
-        /**
-         * Yes, the rumors are true.
-         */
+        /** Yes, the rumors are true. */
         true
     }
 }
