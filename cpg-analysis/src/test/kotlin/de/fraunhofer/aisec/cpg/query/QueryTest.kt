e8f594f42123372b50fb854f1ca1987534fe7e3d
KuechA <31155350+KuechA@users.noreply.github.com>
2025-05-23 16:58:39 +0200

    Implement more operators returning QueryTrees (#2311)
    * Logical operators create query trees
    
    * More such functions
    
    * Null all the things

--- cpg-analysis/src/test/kotlin/de/fraunhofer/aisec/cpg/query/QueryTest.kt
+++ cpg-analysis/src/test/kotlin/de/fraunhofer/aisec/cpg/query/QueryTest.kt
@@ -90,7 +90,7 @@ class QueryTest {
         val queryTreeResult =
             result.allExtended<CallExpression>(
                 mustSatisfy = {
-                    (const("memcpy") eq it.name.localName) implies
+                    ("memcpy" eq it.name.localName) implies
                         (lazy { it.arguments[0].size gt it.arguments[1].size })
                 }
             )
@@ -168,14 +168,14 @@ class QueryTest {
 
         val queryTreeResult =
             result.all<CallExpression>({ it.name.localName == "memcpy" }) {
-                it.arguments[2].intValue!! >= const(11)
+                it.arguments[2].intValue >= 11
             }
         assertTrue(queryTreeResult.first)
 
         val queryTreeResult2 =
             result.allExtended<CallExpression>(
                 { it.name.localName == "memcpy" },
-                { it.arguments[2].intValue!! ge 11 },
+                { it.arguments[2].intValue ge 11 },
             )
 
         assertTrue(queryTreeResult2.value)
@@ -183,7 +183,7 @@ class QueryTest {
         val queryTreeResult3 =
             result.allExtended<CallExpression>(
                 { it.name.localName == "memcpy" },
-                { it.arguments[2].intValue!! ge const(11) },
+                { it.arguments[2].intValue ge 11 },
             )
 
         assertTrue(queryTreeResult3.value)
@@ -195,14 +195,14 @@ class QueryTest {
 
         val queryTreeResult =
             result.all<CallExpression>({ it.name.localName == "memcpy" }) {
-                it.arguments[2].intValue!! > const(11)
+                it.arguments[2].intValue > 11
             }
         assertFalse(queryTreeResult.first)
 
         val queryTreeResult2 =
             result.allExtended<CallExpression>(
                 { it.name.localName == "memcpy" },
-                { it.arguments[2].intValue!! gt 11 },
+                { it.arguments[2].intValue gt 11 },
             )
 
         assertFalse(queryTreeResult2.value)
@@ -210,7 +210,7 @@ class QueryTest {
         val queryTreeResult3 =
             result.allExtended<CallExpression>(
                 { it.name.localName == "memcpy" },
-                { it.arguments[2].intValue!! gt const(11) },
+                { it.arguments[2].intValue gt 11 },
             )
 
         assertFalse(queryTreeResult3.value)
@@ -222,14 +222,14 @@ class QueryTest {
 
         val queryTreeResult =
             result.all<CallExpression>({ it.name.localName == "memcpy" }) {
-                it.arguments[2].intValue!! <= const(11)
+                it.arguments[2].intValue <= 11
             }
         assertTrue(queryTreeResult.first)
 
         val queryTreeResult2 =
             result.allExtended<CallExpression>(
                 { it.name.localName == "memcpy" },
-                { it.arguments[2].intValue!! le 11 },
+                { it.arguments[2].intValue le 11 },
             )
 
         assertTrue(queryTreeResult2.value)
@@ -237,7 +237,7 @@ class QueryTest {
         val queryTreeResult3 =
             result.allExtended<CallExpression>(
                 { it.name.localName == "memcpy" },
-                { it.arguments[2].intValue!! le const(11) },
+                { it.arguments[2].intValue le 11 },
             )
 
         assertTrue(queryTreeResult3.value)
@@ -249,14 +249,14 @@ class QueryTest {
 
         val queryTreeResult =
             result.all<CallExpression>({ it.name.localName == "memcpy" }) {
-                it.arguments[2].intValue!! == const(11)
+                it.arguments[2].intValue?.value == 11
             }
         assertTrue(queryTreeResult.first)
 
         val queryTreeResult2 =
             result.allExtended<CallExpression>(
                 { it.name.localName == "memcpy" },
-                { it.arguments[2].intValue!! eq 11 },
+                { it.arguments[2].intValue eq 11 },
             )
 
         assertTrue(queryTreeResult2.value)
@@ -264,7 +264,7 @@ class QueryTest {
         val queryTreeResult3 =
             result.allExtended<CallExpression>(
                 { it.name.localName == "memcpy" },
-                { it.arguments[2].intValue!! eq const(11) },
+                { it.arguments[2].intValue eq 11 },
             )
 
         assertTrue(queryTreeResult3.value)
@@ -276,14 +276,14 @@ class QueryTest {
 
         val queryTreeResult =
             result.all<CallExpression>({ it.name.localName == "memcpy" }) {
-                it.arguments[2].intValue!! < const(11)
+                it.arguments[2].intValue < 11
             }
         assertFalse(queryTreeResult.first)
 
         val queryTreeResult2 =
             result.allExtended<CallExpression>(
                 { it.name.localName == "memcpy" },
-                { it.arguments[2].intValue!! lt 11 },
+                { it.arguments[2].intValue lt 11 },
             )
 
         assertFalse(queryTreeResult2.value)
@@ -291,7 +291,7 @@ class QueryTest {
         val queryTreeResult3 =
             result.allExtended<CallExpression>(
                 { it.name.localName == "memcpy" },
-                { it.arguments[2].intValue!! lt const(11) },
+                { it.arguments[2].intValue lt 11 },
             )
 
         assertFalse(queryTreeResult3.value)
@@ -303,14 +303,14 @@ class QueryTest {
 
         val queryTreeResult =
             result.all<CallExpression>({ it.name.localName == "memcpy" }) {
-                it.arguments[2].intValue!! != const(11)
+                it.arguments[2].intValue?.value != 11
             }
         assertFalse(queryTreeResult.first)
 
         val queryTreeResult2 =
             result.allExtended<CallExpression>(
                 { it.name.localName == "memcpy" },
-                { it.arguments[2].intValue!! ne 11 },
+                { it.arguments[2].intValue ne 11 },
             )
 
         assertFalse(queryTreeResult2.value)
@@ -318,7 +318,7 @@ class QueryTest {
         val queryTreeResult3 =
             result.allExtended<CallExpression>(
                 { it.name.localName == "memcpy" },
-                { it.arguments[2].intValue!! ne const(11) },
+                { it.arguments[2].intValue ne 11 },
             )
 
         assertFalse(queryTreeResult3.value)
@@ -330,14 +330,14 @@ class QueryTest {
 
         val queryTreeResult =
             result.all<CallExpression>({ it.name.localName == "memcpy" }) {
-                it.arguments[2].intValue!!.value in listOf(11, 2, 3)
+                it.arguments[2].intValue?.value in listOf(11, 2, 3)
             }
         assertTrue(queryTreeResult.first)
 
         val queryTreeResult2 =
             result.allExtended<CallExpression>(
                 { it.name.localName == "memcpy" },
-                { it.arguments[2].intValue!! IN listOf(11, 2, 3) },
+                { it.arguments[2].intValue IN listOf(11, 2, 3) },
             )
 
         assertTrue(queryTreeResult2.value)
@@ -345,7 +345,7 @@ class QueryTest {
         val queryTreeResult3 =
             result.allExtended<CallExpression>(
                 { it.name.localName == "memcpy" },
-                { it.arguments[2].intValue!! IN const(listOf(11, 2, 3)) },
+                { it.arguments[2].intValue IN listOf(11, 2, 3) },
             )
 
         assertTrue(queryTreeResult3.value)
@@ -554,8 +554,8 @@ class QueryTest {
                 { it.assignments.all { assign -> assign.target.type.isPrimitive } },
                 {
                     QueryTree(it.assignments.any {
-                        (max(it.value) le maxSizeOfType(it.target!!.type)) and
-                                (min(it.value) ge minSizeOfType(it.target!!.type))
+                        (max(it.value) le maxSizeOfType(it.target.type)) and
+                                (min(it.value) ge minSizeOfType(it.target.type))
                     })
                 }
             )
