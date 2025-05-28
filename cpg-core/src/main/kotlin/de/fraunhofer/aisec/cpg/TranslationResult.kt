da36a2cfa9a96d973dddca2f9c0b4b6c8ddcc45a
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2025-05-28 15:49:40 +0200

    Adding map of uuid to assumptionstatus (#2326)
    * Adding map of uuid to assumptionstatus
    
    * Spotless
    
    ---------
    
    Co-authored-by: Christian Banse <christian.banse@aisec.fraunhofer.de>

--- cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/TranslationResult.kt
+++ cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/TranslationResult.kt
@@ -28,6 +28,8 @@
 package de.fraunhofer.aisec.cpg
 
 import de.fraunhofer.aisec.cpg.TranslationResult.Companion.DEFAULT_APPLICATION_NAME
+import de.fraunhofer.aisec.cpg.assumptions.Assumption
+import de.fraunhofer.aisec.cpg.assumptions.AssumptionStatus
 import de.fraunhofer.aisec.cpg.frontends.Language
 import de.fraunhofer.aisec.cpg.frontends.LanguageFrontend
 import de.fraunhofer.aisec.cpg.frontends.multiLanguage
@@ -46,6 +48,7 @@ import de.fraunhofer.aisec.cpg.processing.strategy.Strategy
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.reflect.KClass
+import kotlin.uuid.Uuid
 import org.neo4j.ogm.annotation.Relationship
 import org.neo4j.ogm.annotation.Transient
 
@@ -81,6 +84,14 @@ class TranslationResult(
 
     /** Contains all languages that were considered in the translation process. */
     @Transient val usedLanguages = mutableSetOf<Language<*>>()
+
+    /**
+     * This map holds the status assigned to assumptions identified by a specific [Uuid] when being
+     * set manually. The map is empty after a new analysis and can be filled during manual
+     * evaluation of assumptions. The Map is then used when a decision is made based on the
+     * [Assumption].
+     */
+    @Transient val assumptionStates: MutableMap<Uuid, AssumptionStatus> = mutableMapOf()
 
     /**
      * Scratch storage that can be used by passes to store additional information in this result.
@@ -136,7 +147,7 @@ class TranslationResult(
     )
     @Synchronized
     fun addTranslationUnit(tu: TranslationUnitDeclaration) {
-        var application = components[DEFAULT_APPLICATION_NAME]
+        val application = components[DEFAULT_APPLICATION_NAME]
         if (application == null) {
             // No application component exists, but it should be since it is automatically created
             // by the configuration, so something is wrong
