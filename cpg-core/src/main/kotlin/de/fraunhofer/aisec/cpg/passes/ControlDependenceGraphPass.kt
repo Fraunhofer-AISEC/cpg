8b1c3d9d4d833675a2be30f910e5546f7e523e3c
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-08-01 10:17:34 +0200

    move basic block construction logic to own file

--- cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/ControlDependenceGraphPass.kt
+++ cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/ControlDependenceGraphPass.kt
@@ -26,7 +26,6 @@
 package de.fraunhofer.aisec.cpg.passes
 
 import de.fraunhofer.aisec.cpg.TranslationContext
-import de.fraunhofer.aisec.cpg.frontends.HasShortCircuitOperators
 import de.fraunhofer.aisec.cpg.graph.BranchingNode
 import de.fraunhofer.aisec.cpg.graph.Node
 import de.fraunhofer.aisec.cpg.graph.allChildren
@@ -44,7 +43,6 @@ import de.fraunhofer.aisec.cpg.helpers.functional.Lattice
 import de.fraunhofer.aisec.cpg.helpers.functional.MapLattice
 import de.fraunhofer.aisec.cpg.helpers.functional.PowersetLattice
 import de.fraunhofer.aisec.cpg.passes.configuration.DependsOn
-import java.util.IdentityHashMap
 import kotlin.collections.component1
 import kotlin.collections.component2
 
@@ -63,138 +61,6 @@ open class ControlDependenceGraphPass(ctx: TranslationContext) : EOGStarterPass(
 
     override fun cleanup() {
         // Nothing to do
-    }
-
-    /**
-     * Collects all basic blocks starting at the given [startNode]. We identify basic blocks by
-     * iterating through the EOG in O(n) and collecting all nodes which are reachable from the
-     * [startNode]. We identify basic blocks by merges and branches in the EOG but may not consider
-     * [ShortCircuitOperator]s as EOG-splitting nodes if we set [splitOnShortCircuitOperator] to
-     * `false`..
-     *
-     * It returns a triple containing:
-     * 1) The first basic block, which is the one starting at the [startNode].
-     * 2) A collection of all basic blocks which were collected.
-     * 3) A map from [Node] to the [BasicBlock] it belongs to. This is used to find out which basic
-     *    block a node belongs to, which is important when constructing the CDG.
-     *
-     * @param startNode The node to start the collection from, usually a [FunctionDeclaration].
-     * @param splitOnShortCircuitOperator If true, the basic blocks will be split on short-circuit
-     *   operators (e.g., `&&` or `||`). If false, the short-circuit operators will not be
-     *   considered, and the basic blocks will be collected as if they were not splitting up the
-     *   EOG.
-     */
-    fun collectBasicBlocks(
-        startNode: Node,
-        splitOnShortCircuitOperator: Boolean,
-    ): Triple<BasicBlock, Collection<BasicBlock>, Map<Node, BasicBlock>> {
-        val allBasicBlocks = mutableSetOf<BasicBlock>()
-        val firstBB = BasicBlock(startNode = startNode)
-        allBasicBlocks.add(firstBB)
-        val worklist =
-            mutableListOf<Triple<Node, EvaluationOrder?, BasicBlock>>(
-                Triple(startNode, null, firstBB)
-            )
-        val alreadySeen = IdentityHashMap<Node, BasicBlock>()
-
-        while (worklist.isNotEmpty()) {
-            var (currentStartNode, reachingEOGEdge, basicBlock) = worklist.removeFirst()
-            // If we have already seen this node, we can skip it.
-            if (currentStartNode in alreadySeen) {
-                val oldBB = alreadySeen[currentStartNode]
-                // There must be some sort of merge point to arrive here twice, so we add the
-                // reaching basic block to the BB this node belongs to.
-                oldBB?.prevEOG += basicBlock
-                continue
-            }
-
-            if (currentStartNode.prevEOG.size > 1 && currentStartNode != basicBlock.startNode) {
-                // If the currentStartNode is reachable from multiple paths, it starts a new basic
-                // block. currentStartNode is part of the new basic block, so we add it after this
-                // if statement.
-                // Set the end node of the old basic block to the last node on the path
-                basicBlock =
-                    BasicBlock(startNode = currentStartNode).apply {
-                        // ingoingEOGEdges.addAll(currentStartNode.prevEOGEdges)
-                        // Save the relationships between the two basic blocks.
-                        prevEOGEdges.add(basicBlock) {
-                            this.branch = reachingEOGEdge?.branch
-                            this.unreachable = reachingEOGEdge?.unreachable ?: false
-                        }
-                    }
-
-                // Add the newly created basic block to the allBasicBlocks set
-                allBasicBlocks.add(basicBlock)
-            }
-            // Add the basic block to the already seen map for this node
-            alreadySeen[currentStartNode] = basicBlock
-
-            basicBlock.nodes.add(currentStartNode)
-
-            val shortCircuit = currentStartNode.astParent as? ShortCircuitOperator
-            val nextRelevantEOGEdges =
-                if (
-                    !splitOnShortCircuitOperator &&
-                        currentStartNode.nextEOGEdges.size > 1 &&
-                        shortCircuit != null
-                ) {
-                    // For ShortCircuitOperators, we select only the branch which is not a shortcut
-                    // because it's not really a CDG-relevant node, and we want to save the branches
-                    // it introduces.
-                    currentStartNode.nextEOGEdges.filter {
-                        shortCircuit.language !is HasShortCircuitOperators ||
-                            (shortCircuit.language as? HasShortCircuitOperators)?.let {
-                                shortCircuit.operatorCode in it.conjunctiveOperators
-                            } == it.branch ||
-                            it.branch == null
-                    }
-                } else {
-                    currentStartNode.nextEOGEdges
-                }
-
-            if (nextRelevantEOGEdges.size > 1) {
-                // If the currentStartNode splits up into multiple paths, the next nodes start a new
-                // basic block. We already generate this here. But currentStartNode is still part of
-                // the current basic block, so we add it before this if statement.
-                // basicBlock.outgoingEOGEdges.addAll(nextRelevantEOGEdges)
-                worklist.addAll(
-                    nextRelevantEOGEdges.mapNotNull {
-                        if (it.end in alreadySeen) {
-                            alreadySeen[it.end]?.prevEOGEdges?.add(basicBlock) {
-                                this.branch = it.branch
-                                this.unreachable = it.unreachable
-                            }
-                            null
-                        } else {
-                            Triple(
-                                it.end,
-                                it,
-                                BasicBlock(startNode = it.end).apply {
-                                    // ingoingEOGEdges.add(it)
-                                    // Save the relationships between the two basic blocks.
-                                    prevEOGEdges.add(basicBlock) {
-                                        this.branch = it.branch
-                                        this.unreachable = it.unreachable
-                                    }
-                                    // Add the newly created basic block to the allBasicBlocks set
-                                    allBasicBlocks.add(this)
-                                },
-                            )
-                        }
-                    }
-                )
-            } else if (nextRelevantEOGEdges.size == 1) {
-                // If there's max. 1 incoming and max. 1 outgoing path, we can add the
-                // currentStartNode to the current basic block.
-                // If the currentStartNode has only one outgoing path, we can continue with this
-                // path.
-                val nextEdge = nextRelevantEOGEdges.single()
-                worklist.add(Triple(nextEdge.end, nextEdge, basicBlock))
-            } else {
-                // List is empty, nothing to do.
-            }
-        }
-        return Triple(firstBB, allBasicBlocks, alreadySeen)
     }
 
     /**
@@ -409,7 +275,7 @@ fun transfer(
     // branch. In this case, the next node will move "one layer downwards" in the CDG.
     val branchingNode = currentStart.branchingNode
     if (branchingNode != null) {
-        // We start in a branching node and end in one of the branches, so we have thes
+        // We start in a branching node and end in one of the branches, so we have the
         // following state:
         // for the branching node "start", we have a path through "end".
         val prevPathLattice =
