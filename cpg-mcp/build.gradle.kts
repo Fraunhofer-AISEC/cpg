bf1df594dab440f712dcf1bf745211a89d9f0b2a
Wendland, Florian <florian.wendland@aisec.fraunhofer.de>
2025-10-08 19:35:01 +0200

    Add missing dependency for Python language frontend in integration tests

--- cpg-mcp/build.gradle.kts
+++ cpg-mcp/build.gradle.kts
@@ -78,6 +78,12 @@ dependencies {
     integrationTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
     integrationTestImplementation(libs.mcp)
     integrationTestImplementation(libs.ktor.serialization.kotlinx.json)
+    // We depend on the Python frontend for the integration tests, but the frontend is only
+    // available if enabled.
+    // If it's not available, the integration tests fail (which is ok). But if we would directly
+    // reference the project here, the build system would fail any task since it will not find a
+    // non-enabled project.
+    findProject(":cpg-language-python")?.also { integrationTestImplementation(it) }
 
     implementation(project(":cpg-concepts"))
     implementation(project(":cpg-analysis"))
