f157140c3933f47f512ecd69988ad566b8891efe
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2025-10-10 11:25:43 +0200

    Merging namespace skip if it has no location with ast tree exploration if nodes have no location to better match

--- cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/helpers/CommentMatcher.kt
+++ cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/helpers/CommentMatcher.kt
@@ -102,24 +102,22 @@ class CommentMatcher {
                 }
                 .toMutableSet()
 
-        // Because we sometimes wrap all elements into a NamespaceDeclaration we have to extract the
-        // children with a location
-        children.addAll(
-            children.filterIsInstance<NamespaceDeclaration>().flatMap { namespace ->
-                namespace.astChildren.filter { it !in children }
-            }
-        )
-
         // When a child has no location we can not properly consider it for comment matching,
-        // however, it might have
-        // a child with a location that we want to consider, this can overlap with namespaces but
-        // nodes are considered
-        // only once in the set
-        children.addAll(
-            children
-                .filter { node -> node.location == null || node.location?.region == Region() }
-                .flatMap { locationLess -> locationLess.astChildren.filter { it !in children } }
-        )
+        // however, it might have a child with a location that we want to consider. Because the so
+        // explored children
+        // might also have no location we have to repeat this until we have explored all children
+        // and ast leaf nodes are
+        // reached.
+        var locationLess =
+            children.filter { node -> node.location == null || node.location?.region == Region() }
+        while (locationLess.isNotEmpty()) {
+            val containedChildren = locationLess.flatMap { it.astChildren }
+            children.addAll(containedChildren)
+            locationLess =
+                containedChildren
+                    .filter { node -> node.location == null || node.location?.region == Region() }
+                    .filter { it !in children }
+        }
 
         // Searching for the closest successor to our comment amongst the children of the smallest
         // enclosing nodes
