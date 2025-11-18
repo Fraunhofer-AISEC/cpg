367f04d0fc539bc93c1aace07e14545a22020193
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2025-11-18 14:36:25 +0100

    building uniffi with kotlin binding print_string

--- cpg-language-rust/src/main/rust/lib.rs
+++ cpg-language-rust/src/main/rust/lib.rs
@@ -1,17 +1,6 @@
-#![crate_type = "cdylib"]
+uniffi::setup_scaffolding!();
 
-use std::ffi::{CStr};
-use libc::c_char;
-
-#[no_mangle]
-pub extern "C" fn print_file(file: *const c_char) {
-        if !file.is_null() {
-            // return std::ptr::null_mut();
-            let cstr = unsafe { CStr::from_ptr(file) };
-            let src = match cstr.to_str() {
-                Ok(s) => s,
-                Err(_) => "",
-            };
-            println!("Hello, world: {filename}" , filename=src);
-        }
+#[uniffi::export]
+fn print_string(s: String) {
+    println!("{}", s);
 }
