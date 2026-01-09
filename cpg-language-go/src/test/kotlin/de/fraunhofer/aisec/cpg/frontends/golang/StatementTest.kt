7ecb4966c8d7d7e91510899a1babef3aba8fdb4c
Mathias Morbitzer <mathias.morbitzer@aisec.fraunhofer.de>
2026-01-09 16:36:43 +0100

    re-enabled statementTest

--- cpg-language-go/src/test/kotlin/de/fraunhofer/aisec/cpg/frontends/golang/StatementTest.kt
+++ cpg-language-go/src/test/kotlin/de/fraunhofer/aisec/cpg/frontends/golang/StatementTest.kt
@@ -30,14 +30,11 @@ import de.fraunhofer.aisec.cpg.graph.statements.*
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.*
 import de.fraunhofer.aisec.cpg.test.*
 import java.nio.file.Path
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
 import kotlin.test.assertNotNull
 
-// TODO for merge
-@Ignore
 class StatementTest {
 
     @Test
