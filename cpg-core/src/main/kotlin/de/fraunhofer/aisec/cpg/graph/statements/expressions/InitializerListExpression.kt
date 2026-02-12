805e936380afb56dde5733af00cc8077df28732f
KuechA <31155350+KuechA@users.noreply.github.com>
2026-02-12 17:06:31 +0100

    Some improvements for `InitializerListExpression`s (#2555)
    * Check edges only once
    
    * Do not draw the DFG edges in the frontend

--- cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/graph/statements/expressions/InitializerListExpression.kt
+++ cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/graph/statements/expressions/InitializerListExpression.kt
@@ -117,9 +117,7 @@ class InitializerListExpression : Expression(), ArgumentHolder, HasType.TypeObse
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is InitializerListExpression) return false
-        return super.equals(other) &&
-            initializers == other.initializers &&
-            propertyEqualsList(initializerEdges, other.initializerEdges)
+        return super.equals(other) && propertyEqualsList(initializerEdges, other.initializerEdges)
     }
 
     override fun hashCode(): Int {
