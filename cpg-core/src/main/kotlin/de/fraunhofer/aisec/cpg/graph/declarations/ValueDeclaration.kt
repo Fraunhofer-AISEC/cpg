3cd626cf0d539baa6b46a77831c3ce5077dbea10
Christian Banse <christian.banse@aisec.fraunhofer.de>
2026-02-19 17:25:05 +0000

    Moving `isStatic` to `ValueDeclaration`
    For some reason, only `MethodDeclaration` had a static field. But other value declarations, such as fields and variables can be static.

--- cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/graph/declarations/ValueDeclaration.kt
+++ cpg-core/src/main/kotlin/de/fraunhofer/aisec/cpg/graph/declarations/ValueDeclaration.kt
@@ -99,6 +99,12 @@ abstract class ValueDeclaration : Declaration(), HasType, HasAliases {
     /** All usages of the variable/field. */
     @PopulatedByPass(SymbolResolver::class) var usages by unwrapping(ValueDeclaration::usageEdges)
 
+    /**
+     * Defines, whether this declaration is static or not. Commonly used to declare static fields or
+     * methods in classes.
+     */
+    var isStatic = false
+
     override fun toString(): String {
         return ToStringBuilder(this, TO_STRING_STYLE).appendSuper(super.toString()).toString()
     }
