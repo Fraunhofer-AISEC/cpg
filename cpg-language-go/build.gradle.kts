fb660b0152c71705bec1467c678054c8fb0af1da
Tobias Specht <tobias.specht@aisec.fraunhofer.de>
2026-04-08 09:59:50 +0200

    Update libgoast to v0.0.7 (#2692)

--- cpg-language-go/build.gradle.kts
+++ cpg-language-go/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
 tasks {
     val downloadLibGoAST by
         registering(Download::class) {
-            val version = "v0.0.6"
+            val version = "v0.0.7"
 
             src(
                 listOf(
