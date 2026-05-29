299ba5b5b65f6cf24b972ee5f3141794d42384cb
Mathias Morbitzer <mathias.morbitzer@aisec.fraunhofer.de>
2026-05-29 16:22:53 +0200

    fixed next test

--- cpg-core/src/test/kotlin/de/fraunhofer/aisec/cpg/enhancements/DFGFunctionSummariesTest.kt
+++ cpg-core/src/test/kotlin/de/fraunhofer/aisec/cpg/enhancements/DFGFunctionSummariesTest.kt
@@ -35,7 +35,6 @@ import de.fraunhofer.aisec.cpg.graph.builder.*
 import de.fraunhofer.aisec.cpg.graph.edges.flows.CallingContextIn
 import de.fraunhofer.aisec.cpg.graph.edges.flows.CallingContextOut
 import de.fraunhofer.aisec.cpg.graph.edges.flows.ContextSensitiveDataflow
-import de.fraunhofer.aisec.cpg.graph.edges.flows.PointerDataflowGranularity
 import de.fraunhofer.aisec.cpg.graph.expressions.ParameterMemoryValue
 import de.fraunhofer.aisec.cpg.graph.expressions.Reference
 import de.fraunhofer.aisec.cpg.graph.expressions.Return
@@ -263,10 +262,7 @@ class DFGFunctionSummariesTest {
         assertEquals(1, argA.nextDFG.size)
         // The MemoryAddress a
         assertEquals(1, argA.prevFullDFG.size)
-        assertEquals(
-            1,
-            argA.prevDFGEdges.filter { it.granularity is PointerDataflowGranularity }.size,
-        )
+        assertEquals(1, argA.prevDFGEdges.filter { it.derefDepth != null }.size)
 
         val nextDfg = argA.nextDFGEdges.single()
         assertEquals(
