62f5503c33916936cc1fd6b3adfe97b2899afb29
Mathias Morbitzer <mathias.morbitzer@aisec.fraunhofer.de>
2026-09-02 10:27:55 +0200

    fixed test

--- cpg-core/src/test/kotlin/de/fraunhofer/aisec/cpg/passes/ControlDependenceGraphPassTest.kt
+++ cpg-core/src/test/kotlin/de/fraunhofer/aisec/cpg/passes/ControlDependenceGraphPassTest.kt
@@ -40,6 +40,7 @@ import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
+import kotlin.time.Duration
 import org.junit.jupiter.api.assertInstanceOf
 
 class ControlDependenceGraphPassTest {
@@ -307,7 +308,7 @@ class ControlDependenceGraphPassTest {
                         .defaultPasses()
                         .registerPass<ControlDependenceGraphPass>()
                         .configurePass<ControlDependenceGraphPass>(
-                            ControlDependenceGraphPass.Configuration(timeout = 0L)
+                            ControlDependenceGraphPass.Configuration(timeout = Duration.INFINITE)
                         )
                         .build()
                 )
